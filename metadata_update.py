fe3d6eed703cf41685dee5a61f665960f0c69e94
Adriane Hanson <ahansonuga@gmail.com>
2024-10-08 16:37:07 -0400

    Create metadata_update.py

---
+++ metadata_update.py
@@ -0,0 +1,15 @@
+"""
+Draft script to update the metadata file from a css/cms export to make it more usable.
+"""
+import pandas as pd
+import sys
+
+if __name__ == '__main__':
+    # Gets the path to the metadata file from the script argument.
+    # TODO: error checking
+    md_path = sys.argv[1]
+
+    # Reads the metadata file into a pandas dataframe.
+    # TODO: limiting the number of rows for testing to avoid a ParserError within the data
+    # TODO: document the encoding errors?
+    df = pd.read_csv(md_path, delimiter="\t", nrows=1000, encoding_errors="ignore")
