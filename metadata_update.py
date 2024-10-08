b828e05ddb7633e916d92ffa6ffd06a8b9925cac
Adriane Hanson <ahansonuga@gmail.com>
2024-10-08 16:44:49 -0400

    Remove personal id columns

--- metadata_update.py
+++ metadata_update.py
@@ -13,3 +13,9 @@ if __name__ == '__main__':
     # TODO: limiting the number of rows for testing to avoid a ParserError within the data
     # TODO: document the encoding errors?
     df = pd.read_csv(md_path, delimiter="\t", nrows=1000, encoding_errors="ignore")
+
+    # Removes the personal identifying columns from the dataframe, if they are present.
+    # Nothing happens, due to errors="ignore", if any are not present.
+    remove = ['prefix', 'first', 'middle', 'last', 'suffix', 'appellation', 'title', 'org',
+              'addr1', 'addr2', 'addr3', 'addr4', 'city', 'state', 'extra']
+    df = df.drop(remove, axis=1, errors="ignore")
