1b91f2f8e08ba5c9e51c734e05d70a264a8a57af
Adriane Hanson <ahansonuga@gmail.com>
2024-10-08 16:50:34 -0400

    Print remaining columns

--- metadata_update.py
+++ metadata_update.py
@@ -19,3 +19,10 @@ if __name__ == '__main__':
     remove = ['prefix', 'first', 'middle', 'last', 'suffix', 'appellation', 'title', 'org',
               'addr1', 'addr2', 'addr3', 'addr4', 'city', 'state', 'extra']
     df = df.drop(remove, axis=1, errors="ignore")
+
+    # Prints the remaining columns for archivist review, in case any additional ones might contain private information.
+    print("\nColumns remaining in the constituent mail metadata after removing personal identifiers are listed below.")
+    print("To remove any of these columns from the metadata, add them to the 'remove' list and run the script again.")
+    for column_name in df.columns.tolist():
+        print('\t', column_name)
+
