2e99273dcccbe3138481c01726e499c5ddf5fce1
Adriane Hanson <ahansonuga@gmail.com>
2024-10-09 16:48:00 -0400

    Create test_read_metadata.py

---
+++ tests/test_read_metadata.py
@@ -0,0 +1,19 @@
+"""
+Tests for the function read_metadata(), which reads the DAT file into a pandas dataframe.
+"""
+import os
+import unittest
+from metadata_update import read_metadata
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_parsererror(self):
+        """Test for when the DAT file has content with tabs, resulting in a ParserError without correction"""
+        md_df = read_metadata(os.path.join('test_data', 'read_parsererror_md.dat'))
+
+        self.assertEqual(True, True)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
