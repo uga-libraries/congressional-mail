8d245caa0c81eb12bc25c4eb0b4438e3779cd33b
Adriane Hanson <ahansonuga@gmail.com>
2024-10-21 12:57:05 -0400

    Create read_metadata()

--- archival_office_correspondence_data.py
+++ archival_office_correspondence_data.py
@@ -7,7 +7,31 @@ import sys
 from css_archiving_format import check_argument, save_df, split_congress_year
 
 
-if __name__ == 'main':
+def read_metadata(path):
+    """Read the metadata file into a dataframe"""
+
+    # Makes a list from the file contents with one list per row and one item per column,
+    # splitting the data into columns based on the character position and removing extra spaces.
+    # TODO: original data can be all caps. Should we change the case or leave it?
+    rows_list = []
+    positions = [(0, 39), (39, 69), (69, 99), (99, 129), (129, 159), (159, 189), (189, 191), (191, 201), (201, 251),
+                 (251, 301), (301, 351), (351, 357), (357, 361), (361, 371), (371, 471)]
+    with open(path) as open_file:
+        for line in open_file:
+            row_list = [line[slice(*pos)].strip() for pos in positions]
+            rows_list.append(row_list)
+
+    # Save as a dataframe, with column names.
+    # TODO: verify these column names.
+    columns_list = ['name', 'title', 'organization', 'address_line_1', 'address_line_2', 'city', 'state_code',
+                    'zip_code', 'correspondence_type', 'correspondence_topic', 'correspondence_subtopic',
+                    'letter_date', 'staffer_initials', 'document_number', 'comments']
+    df = pd.DataFrame(rows_list, columns=columns_list, dtype=str)
+
+    return df
+
+
+if __name__ == '__main__':
 
     # Gets the path to the metadata file from the script argument.
     # If it is missing or not a valid path, prints an error and exits the script.
@@ -17,6 +41,7 @@ if __name__ == 'main':
         sys.exit(1)
 
     # Reads the metadata file into a pandas dataframe.
+    md_df = read_metadata(md_path)
 
     # Removes columns with personally identifiable information, if they are present.
 
---
+++ tests/archival_office/test_read_metadata.py
@@ -0,0 +1,33 @@
+"""
+Tests for the function read_metadata(), which reads the dat file into a dataframe.
+"""
+import os
+import unittest
+from archival_office_correspondence_data import read_metadata
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_function(self):
+        """Test for when the function works correctly"""
+        # TODO: decide on variations and make input separate from the script
+        md_df = read_metadata(os.path.join('test_data', 'script_md.dat'))
+
+        md_df.fillna('', inplace=True)
+        result = [md_df.columns.tolist()] + md_df.values.tolist()
+        expected = [['name', 'title', 'organization', 'address_line_1', 'address_line_2', 'city', 'state_code',
+                     'zip_code', 'correspondence_type', 'correspondence_topic', 'correspondence_subtopic',
+                     'letter_date', 'staffer_initials', 'document_number', 'comments'],
+                    ['', '', 'COMPANY ABC', '123 MAIN', '', 'CITY A', 'GA', '30000', 'ISSUE', 'CO-OLD',
+                     '', '971002', 'ABC', '123XYZ456', ''],
+                    [',  ,', 'EDITOR', 'NEWSPAPER GAZETTE', 'PO BOX 456', '999 BLVD', 'CITY B', 'FL', '70000', 'ISSUE',
+                     'PR-GEN', '', '971003', 'ABC', '', 'NOTE'],
+                    ['LAST A, FIRST A, MR.', '', '', '1001 1ST STREET', '', 'CITY A', 'GA', '30000-1234',
+                     'CASE', 'TR-HWY', '', '', 'RA', '555QRS667', 'ANOTHER NOTE'],
+                    ['LAST B, FIRST B MIDDLE, JR.', '', '', '200 ROAD', 'APT 5B', 'CITY A', 'GA', '30001',
+                     'ISSUE', 'TR-RAL', '', '980104', 'NOP', '', '']]
+        self.assertEqual(result, expected, "Problem with test for function")
+
+
+if __name__ == '__main__':
+    unittest.main()
