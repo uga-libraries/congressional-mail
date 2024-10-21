7389c117e87fa23ad6c6eccde595f2b57038c4aa
Adriane Hanson <ahansonuga@gmail.com>
2024-10-21 16:08:07 -0400

    Create split_congress_year()

--- archival_office_correspondence_data.py
+++ archival_office_correspondence_data.py
@@ -52,6 +52,42 @@ def remove_pii(df):
     return df
 
 
+def split_congress_year(df, input_dir):
+    """Make one CSV per Congress Year in the folder with the original metadata file"""
+
+    # Saves rows without a year (date is a not a number, could be blank or text) to a CSV.
+    # TODO: confirm that text in place of date should be in undated: usually an error in the number of columns.
+    # TODO: confirm if should have a maximum size, for ones that are still too large to open in a spreadsheet.
+    # TODO: decide on file name and where it saves.
+    df_undated = df[pd.to_numeric(df['letter_date'], errors='coerce').isnull()]
+    df_undated.to_csv(os.path.join(input_dir, 'undated.csv'), index=False)
+
+    # Removes rows without a year from the dataframe, so the rest can be split by Congress Year.
+    df = df[pd.to_numeric(df['letter_date'], errors='coerce').notnull()].copy()
+
+    # Adds a column with the year received, which will be used to calculate the Congress Year.
+    # Column letter_date is formatted YYMMDD.
+    # First the two digit year is extracted, and then it is made a four-digit year by adding 1900 or 2000.
+    # TODO: confirm 1960 as the cut off for 1900s vs 2000s.
+    df.loc[:, 'year'] = df['letter_date'].astype(str).str[:2].astype(int)
+    df.loc[df['year'] >= 60, 'year'] = df['year'] + 1900
+    df.loc[df['year'] < 60, 'year'] = df['year'] + 2000
+
+    # Adds a column with the Congress Year received, which is a two-year range starting with an odd year.
+    # First, if the year received is even, the Congress Year is year-1 to year.
+    # Second, if the year received is odd, the Congress Year is year to year+1.
+    df.loc[df['year'] % 2 == 0, 'congress_year'] = (df['year'] - 1).astype(str) + '-' + df['year'].astype(str)
+    df.loc[df['year'] % 2 == 1, 'congress_year'] = df['year'].astype(str) + '-' + (df['year'] + 1).astype(str)
+
+    # Splits the data by Congress Year received and saves each to a separate CSV.
+    # The year and congress_year columns are first removed, so the CSV only has the original columns.
+    # TODO: decide on file name and where it saves.
+    # TODO: confirm using CSV format.
+    for congress_year, cy_df in df.groupby('congress_year'):
+        cy_df = cy_df.drop(['year', 'congress_year'], axis=1)
+        cy_df.to_csv(os.path.join(input_dir, f'{congress_year}.csv'), index=False)
+
+
 if __name__ == '__main__':
 
     # Gets the path to the metadata file from the script argument.
@@ -68,7 +104,7 @@ if __name__ == '__main__':
     md_df = remove_pii(md_df)
 
     # Saves the redacted data to a CSV file in the folder with the original metadata file.
-    # save_df(md_df, os.path.dirname(md_path))
+    save_df(md_df, os.path.dirname(md_path))
 
     # Saves a copy of the redacted data to one CSV per Congress Year in the folder with the original metadata file.
     # split_congress_year(md_df, os.path.dirname(md_path))
