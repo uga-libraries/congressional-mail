03f0f97ee889d00ad76faef97518a4fedfc475c3
Adriane Hanson <ahansonuga@gmail.com>
2024-10-28 14:38:29 -0400

    Remove unnecessary stdout
    For when the testing what the script prints

--- tests/css_data_interchange_format/test_script.py
+++ tests/css_data_interchange_format/test_script.py
@@ -30,7 +30,7 @@ class MyTestCase(unittest.TestCase):
         # Runs the script.
         script_path = os.path.join(os.getcwd(), '..', '..', 'css_data_interchange_format.py')
         md_path = os.path.join('test_data', 'script')
-        output = subprocess.run(f"python {script_path} {md_path}", shell=True, stdout=subprocess.PIPE)
+        subprocess.run(f"python {script_path} {md_path}", shell=True)
 
         # Tests the contents of CSS_Access_Copy.csv.
         csv_path = os.path.join('test_data', 'CSS_Access_Copy.csv')
