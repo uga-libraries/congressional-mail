9f8db241a97babd8a88320ee7a4bd6c9474860c5
Adriane Hanson <ahansonuga@gmail.com>
2024-10-28 10:32:12 -0400

    Add extra columns to all test

--- tests/css_data_interchange_format/test_remove_pii.py
+++ tests/css_data_interchange_format/test_remove_pii.py
@@ -17,30 +17,31 @@ class MyTestCase(unittest.TestCase):
                                '10', '11', '12', '13', '14', '15', '16', '17', '18', '19',
                                '20', '21', '22', '23', '24', '25', '26', '27', '28', '29',
                                '30', '31', '32', '33', '34', '35', '36', '37', '38', '39',
-                               '40', '41', '42', '43', '44', '45', '46', '47', '48']],
+                               '40', '41', '42', '43', '44', '45', '46', '47', '48', '49',
+                               '50', '51', '52', '53']],
                              columns=['record_type_x', 'person_id_x', 'address_id_x', 'address_type', 'primary_flag',
                                       'default_address_flag', 'title', 'organization_name', 'address_line_1',
                                       'address_line_2', 'address_line_3', 'address_line_4', 'city', 'state_code',
                                       'zip_code', 'carrier_route', 'county', 'country', 'district', 'precinct',
-                                      'no_mail_flag', 'deliverability', 'record_type_y', 'communication_id',
-                                      'workflow_id', 'workflow_person_id', 'communication_type', 'user_id',
-                                      'approved_by', 'status', 'date_in', 'date_out', 'reminder_date', 'update_date',
-                                      'response_type', 'address_id_y', 'email_address', 'household_flag',
-                                      'household_id', 'group_name', 'salutation', 'record_type', 'person_id_y',
-                                      'document_type', 'communication_document_name', 'communication_document_id',
-                                      'file_location', 'file_name'])
+                                      'no_mail_flag', 'deliverability', 'extra1', 'extra2', 'extra3', 'extra4',
+                                      'record_type_y', 'communication_id', 'workflow_id', 'workflow_person_id',
+                                      'communication_type', 'user_id', 'approved_by', 'status', 'date_in', 'date_out',
+                                      'reminder_date', 'update_date', 'response_type', 'address_id_y', 'email_address',
+                                      'household_flag', 'household_id', 'group_name', 'salutation', 'extra',
+                                      'record_type', 'person_id_y', 'document_type', 'communication_document_name',
+                                      'communication_document_id', 'file_location', 'file_name'])
         md_df = remove_pii(md_df)
 
         # Tests the columns of the returned dataframe are correct.
         expected = ['city', 'state_code', 'zip_code', 'country', 'communication_type', 'approved_by', 'status',
                     'date_in', 'date_out', 'reminder_date', 'update_date', 'response_type', 'group_name',
-                    'document_type', 'communication_document_name', 'communication_document_id', 'file_location',
-                    'file_name']
+                    'document_type', 'communication_document_name', 'communication_document_id',
+                    'file_location', 'file_name']
         self.assertEqual(md_df.columns.tolist(), expected, "Problem with test for all present, columns")
 
         # Tests the values in the returned dataframe are correct.
-        expected = [['13', '14', '15', '18', '27', '29', '30', '31', '32', '33', '34', '35',
-                     '40', '44', '45', '46', '47', '48']]
+        expected = [['13', '14', '15', '18', '31', '33', '34', '35', '36', '37', '38', '39',
+                     '44', '49', '50', '51', '52', '53']]
         self.assertEqual(md_df.values.tolist(), expected, "Problem with test for all present, values")
 
     def test_some_present(self):
