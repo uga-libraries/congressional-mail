8a61c47d163c71ab63723e66767d34476af4b379
Adriane Hanson <ahansonuga@gmail.com>
2024-11-22 14:41:32 -0500

    Use output_dir to shorten paths

--- tests/css_archiving_format/test_remove_casework_letters.py
+++ tests/css_archiving_format/test_remove_casework_letters.py
@@ -21,26 +21,29 @@ def files_in_dir(dir_path):
 class MyTestCase(unittest.TestCase):
 
     def tearDown(self):
-        if os.path.exists(os.path.join('test_data', 'remove_casework_letters', 'correct', 'css_export')):
-            shutil.rmtree(os.path.join('test_data', 'remove_casework_letters', 'correct', 'css_export'))
+        output_dir = os.path.join('test_data', 'remove_casework_letters', 'correct')
+
+        if os.path.exists(os.path.join(output_dir, 'css_export')):
+            shutil.rmtree(os.path.join(output_dir, 'css_export'))
 
         today = date.today().strftime('%Y-%m-%d')
-        if os.path.exists(os.path.join('test_data', 'remove_casework_letters', f'file_deletion_log_{today}.csv')):
-            os.remove(os.path.join('test_data', 'remove_casework_letters', f'file_deletion_log_{today}.csv'))
+        if os.path.exists(os.path.join(output_dir, f'file_deletion_log_{today}.csv')):
+            os.remove(os.path.join(output_dir, f'file_deletion_log_{today}.csv'))
 
     def test_function(self):
         """Initial test for the development of the function"""
         # Makes a copy of the test data in the repo, since the script alters the data.
-        shutil.copytree(os.path.join('test_data', 'remove_casework_letters', 'correct', 'css_export_copy'),
-                        os.path.join('test_data', 'remove_casework_letters', 'correct', 'css_export'))
+        output_dir = os.path.join('test_data', 'remove_casework_letters', 'correct')
+        shutil.copytree(os.path.join(output_dir, 'css_export_copy'),
+                        os.path.join(output_dir, 'css_export'))
 
         # Runs the function being tested.
-        input_directory = os.path.join('test_data', 'remove_casework_letters', 'correct', 'css_export')
+        input_directory = os.path.join(output_dir, 'css_export')
         remove_casework_letters(input_directory)
 
         # Tests the contents of the file deletion log.
         today = date.today().strftime('%Y-%m-%d')
-        log_path = os.path.join('test_data', 'remove_casework_letters', 'correct', f'file_deletion_log_{today}.csv')
+        log_path = os.path.join(output_dir, f'file_deletion_log_{today}.csv')
         result = csv_to_list(log_path)
         expected = [['File', 'SizeKB', 'DateCreated', 'DateDeleted', 'MD5', 'Notes'],
                     [r'..\documents\BlobExport\objects\111111.txt'.replace('..', input_directory),
@@ -59,7 +62,7 @@ class MyTestCase(unittest.TestCase):
         result = files_in_dir(input_directory)
         expected = ['form_a.txt', 'test.txt', '100.txt']
         self.assertEqual(result, expected, "Problem with test for directory contents")
-        
+
 
 if __name__ == '__main__':
     unittest.main()
