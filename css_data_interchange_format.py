d322381fcfb5773e1b04762e18078023f6206766
Adriane Hanson <ahansonuga@gmail.com>
2024-12-02 14:57:04 -0500

    Save metadata deletion to one log

--- css_data_interchange_format.py
+++ css_data_interchange_format.py
@@ -83,26 +83,28 @@ def read_metadata(paths):
 
 
 def remove_casework(df, output_dir):
-    """Remove rows with topics or text that indicate they are case mail"""
+    """Remove rows with topics or text that indicate they are casework and log results"""
+
+    # Deletion log path (used multiple times)
+    del_log = os.path.join(output_dir, 'metadata_deletion_log.csv')
 
     # Removes row if column group_name starts with "CASE".
     # There are other groups which included "case" that are retained, referring to legal cases of national interest.
     # Deleted rows are saved to a log for review.
-    # TODO: combine deleted content into a single log.
     group = df['group_name'].str.startswith('CASE', na=False)
-    df[group].to_csv(os.path.join(output_dir, 'group_deletion_log.csv'), index=False)
+    df[group].to_csv(os.path.join(del_log), index=False)
     df = df[~group]
 
     # Removes row if any column includes the text "casework".
-    # Deleted rows are saved to a log for review.
+    # Deleted rows, if any, are saved to a log for review.
     includes_casework = np.column_stack([df[col].str.contains('casework', case=False, na=False) for col in df])
-    df.loc[includes_casework.any(axis=1)].to_csv(os.path.join(output_dir, 'casework_deletion_log.csv'), index=False)
+    df.loc[includes_casework.any(axis=1)].to_csv(del_log, mode='a', header=not os.path.exists(del_log), index=False)
     df = df.loc[~includes_casework.any(axis=1)]
 
     # Remaining rows with "case" in any column are saved to a log for review.
     # This may show us another pattern that indicates casework or may be another use of the word case.
     includes_case = np.column_stack([df[col].str.contains('case', case=False, na=False) for col in df])
-    df.loc[includes_case.any(axis=1)].to_csv(os.path.join(output_dir, 'row_includes_case_log.csv'), index=False)
+    df.loc[includes_case.any(axis=1)].to_csv(os.path.join(output_dir, 'case_remains_log.csv'), index=False)
 
     return df
 
