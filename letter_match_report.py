10bf8b8e1426157d75ffe907b9171935f5043f30
Adriane Hanson <ahansonuga@gmail.com>
2025-01-03 11:21:27 -0500

    Create letter_match_report.py

---
+++ letter_match_report.py
@@ -0,0 +1,12 @@
+"""
+Temporary script to determine how many of the letters match the exported metadata.
+One argument: input directory. Assuming it will be present.
+If decide to keep, will need to generalize more.
+"""
+import sys
+import css_archiving_format as css_a
+
+if __name__ == '__main__':
+    input_dir = sys.argv[1]
+
+    # CSS Archiving Format
