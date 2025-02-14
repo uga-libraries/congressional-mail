ff2ca57a436e2100b1f2a5c281becd4bacd89707
Adriane Hanson <ahansonuga@gmail.com>
2025-02-14 16:21:29 -0500

    Remove BlobExport from expected paths

--- tests/css_archiving_format/test_script.py
+++ tests/css_archiving_format/test_script.py
@@ -64,12 +64,12 @@ class MyTestCase(unittest.TestCase):
                      'out_date', 'out_topic', 'out_text', 'out_document_name', 'out_fillin'],
                     ['Ms.', 'Diane', 'D.', 'Dudly', 'nan', 'nan', 'nan', 'nan', '456 D St', 'nan', 'nan', 'nan',
                      'D city', 'DE', '45678', 'nan', 'd100', 'General', 'Email', '20210101', 'Prison Case',
-                     'nan', r'..\documents\BlobExport\objects\444444.txt', 'nan', 'r400', 'General', 'Email',
-                     '20210111', 'D', 'nan', r'..\documents\BlobExport\indivletters\000004.txt', 'nan'],
+                     'nan', r'..\documents\objects\444444.txt', 'nan', 'r400', 'General', 'Email',
+                     '20210111', 'D', 'nan', r'..\documents\indivletters\000004.txt', 'nan'],
                     ['Ms.', 'Emma', 'E.', 'Evans', 'nan', 'nan', 'nan', 'nan', '567 E St', 'nan', 'nan', 'nan',
                      'E city', 'ME', '56789', 'nan', 'e100', 'General', 'Email', '20210101', 'Casework Issues',
-                     'nan', r'..\documents\BlobExport\objects\555555.txt', 'nan', 'r500', 'General', 'Email',
-                     '20210111', 'E', 'nan', r'..\documents\BlobExport\indivletters\000005.txt', 'nan']]
+                     'nan', r'..\documents\objects\555555.txt', 'nan', 'r500', 'General', 'Email',
+                     '20210111', 'E', 'nan', r'..\documents\indivletters\000005.txt', 'nan']]
         self.assertEqual(result, expected, "Problem with test for access, case delete log")
 
         # Tests that no case remains log was made.
@@ -83,14 +83,14 @@ class MyTestCase(unittest.TestCase):
                      'in_topic', 'in_text', 'in_document_name', 'in_fillin', 'out_id', 'out_type', 'out_method',
                      'out_date', 'out_topic', 'out_text', 'out_document_name', 'out_fillin'],
                     ['A city', 'AL', '12345', 'nan', 'a100', 'General', 'Email', '20210101', 'A1', 'nan',
-                     r'..\documents\BlobExport\objects\111111.txt', 'nan', 'r100', 'General', 'Email', '20210111',
-                     'A', 'nan', r'..\documents\BlobExport\formletters\A', 'nan'],
+                     r'..\documents\objects\111111.txt', 'nan', 'r100', 'General', 'Email', '20210111',
+                     'A', 'nan', r'..\documents\formletters\A', 'nan'],
                     ['B city', 'WY', '23456', 'nan', 'b200', 'General', 'Email', '20230202', 'B1^B2', 'Note',
-                     r'..\documents\BlobExport\objects\222222.txt', 'nan', 'r200', 'General', 'Email', '20230212',
-                     'B', 'nan', r'..\documents\BlobExport\formletters\B', 'nan'],
+                     r'..\documents\objects\222222.txt', 'nan', 'r200', 'General', 'Email', '20230212',
+                     'B', 'nan', r'..\documents\formletters\B', 'nan'],
                     ['C city', 'CO', '34567', 'nan', 'c300', 'General', 'Letter', '20240303', 'A1', 'nan',
-                     r'..\documents\BlobExport\objects\333333.txt', 'nan', 'r300', 'General', 'Email', '20240313',
-                     'A', 'nan', r'..\documents\BlobExport\formletters\A', 'nan']]
+                     r'..\documents\objects\333333.txt', 'nan', 'r300', 'General', 'Email', '20240313',
+                     'A', 'nan', r'..\documents\formletters\A', 'nan']]
         self.assertEqual(result, expected, "Problem with test for access, archiving_correspondence_redacted.csv")
 
         # Tests the contents of 2021-2022.csv.
@@ -100,8 +100,8 @@ class MyTestCase(unittest.TestCase):
                      'in_topic', 'in_text', 'in_document_name', 'in_fillin', 'out_id', 'out_type', 'out_method',
                      'out_date', 'out_topic', 'out_text', 'out_document_name', 'out_fillin'],
                     ['A city', 'AL', '12345', 'nan', 'a100', 'General', 'Email', '20210101', 'A1', 'nan',
-                     r'..\documents\BlobExport\objects\111111.txt', 'nan', 'r100', 'General', 'Email', '20210111',
-                     'A', 'nan', r'..\documents\BlobExport\formletters\A', 'nan']]
+                     r'..\documents\objects\111111.txt', 'nan', 'r100', 'General', 'Email', '20210111',
+                     'A', 'nan', r'..\documents\formletters\A', 'nan']]
         self.assertEqual(result, expected, "Problem with test for access, 2021-2022.csv")
 
         # Tests the contents of 2023-2024.csv.
@@ -111,11 +111,11 @@ class MyTestCase(unittest.TestCase):
                      'in_topic', 'in_text', 'in_document_name', 'in_fillin', 'out_id', 'out_type', 'out_method',
                      'out_date', 'out_topic', 'out_text', 'out_document_name', 'out_fillin'],
                     ['B city', 'WY', '23456', 'nan', 'b200', 'General', 'Email', '20230202', 'B1^B2', 'Note',
-                     r'..\documents\BlobExport\objects\222222.txt', 'nan', 'r200', 'General', 'Email', '20230212',
-                     'B', 'nan', r'..\documents\BlobExport\formletters\B', 'nan'],
+                     r'..\documents\objects\222222.txt', 'nan', 'r200', 'General', 'Email', '20230212',
+                     'B', 'nan', r'..\documents\formletters\B', 'nan'],
                     ['C city', 'CO', '34567', 'nan', 'c300', 'General', 'Letter', '20240303', 'A1', 'nan',
-                     r'..\documents\BlobExport\objects\333333.txt', 'nan', 'r300', 'General', 'Email', '20240313',
-                     'A', 'nan', r'..\documents\BlobExport\formletters\A', 'nan']]
+                     r'..\documents\objects\333333.txt', 'nan', 'r300', 'General', 'Email', '20240313',
+                     'A', 'nan', r'..\documents\formletters\A', 'nan']]
         self.assertEqual(result, expected, "Problem with test for access, 2023-2024.csv")
 
         # Tests that no undated.csv was made.
@@ -142,24 +142,24 @@ class MyTestCase(unittest.TestCase):
                      'out_date', 'out_topic', 'out_text', 'out_document_name', 'out_fillin'],
                     ['Ms.', 'Diane', 'D.', 'Dudly', 'nan', 'nan', 'nan', 'nan', '456 D St', 'nan', 'nan', 'nan',
                      'D city', 'DE', '45678', 'nan', 'd100', 'General', 'Email', '20210101', 'Prison Case', 'nan',
-                     r'..\documents\BlobExport\objects\444444.txt', 'nan', 'r400', 'General', 'Email', '20210111',
-                     'D', 'nan', r'..\documents\BlobExport\formletters\D.txt', 'nan'],
+                     r'..\documents\objects\444444.txt', 'nan', 'r400', 'General', 'Email', '20210111',
+                     'D', 'nan', r'..\documents\formletters\D.txt', 'nan'],
                     ['Ms.', 'Emma', 'E.', 'Evans', 'nan', 'nan', 'nan', 'nan', '567 E St', 'nan', 'nan', 'nan',
                      'E city', 'ME', '56789', 'nan', 'e100', 'General', 'Email', '20210101', 'Casework Issues', 'nan',
-                     r'..\documents\BlobExport\objects\555555.txt', 'nan', 'r500', 'General', 'Email', '20210111',
-                     'E', 'nan', r'..\documents\BlobExport\indivletters\000005.txt', 'nan'],
+                     r'..\documents\objects\555555.txt', 'nan', 'r500', 'General', 'Email', '20210111',
+                     'E', 'nan', r'..\documents\indivletters\000005.txt', 'nan'],
                     ['Ms.', 'Fiona', 'F.', 'Fowler', 'nan', 'nan', 'nan', 'nan', '678 F St', 'nan', 'nan', 'nan',
                      'F city', 'FL', '67890', 'nan', 'f100', 'General', 'Email', '20210101',
                      'Social Security^Casework', 'nan', 'nan', 'nan', 'r600',
-                     'General', 'Email', '20210111', 'F', 'nan', r'..\documents\BlobExport\formletters\F.txt', 'nan'],
+                     'General', 'Email', '20210111', 'F', 'nan', r'..\documents\formletters\F.txt', 'nan'],
                     ['Ms.', 'Ann', 'A.', 'Anderson', 'nan', 'MD', 'nan', 'nan', '123 A St', 'nan', 'nan', 'nan',
                      'A city', 'AL', '12345', 'nan', 'a100', 'General', 'Email', '20210101', 'A1', 'This is casework',
-                     r'..\documents\BlobExport\objects\111111.txt', 'nan', 'r100', 'General', 'Email', '20210111',
-                     'nan', 'nan', r'..\documents\BlobExport\indivletters\000001.txt', 'nan'],
+                     r'..\documents\objects\111111.txt', 'nan', 'r100', 'General', 'Email', '20210111',
+                     'nan', 'nan', r'..\documents\indivletters\000001.txt', 'nan'],
                     ['Mr.', 'Clive', 'C.', 'Cooper', 'Jr.', 'nan', 'CEO', 'Company', 'Attn: C', 'Division', 'POBox',
                      '345 C St', 'C city', 'CO', '34567', 'nan', 'c300', 'General', 'Letter', '20240303', 'C1',
-                     'Maybe casework', r'..\documents\BlobExport\objects\333333.txt', 'nan', 'r300', 'General',
-                     'Email', '20240313', 'C', 'nan', r'..\documents\BlobExport\indivletters\000003.txt', 'nan']]
+                     'Maybe casework', r'..\documents\objects\333333.txt', 'nan', 'r300', 'General',
+                     'Email', '20240313', 'C', 'nan', r'..\documents\indivletters\000003.txt', 'nan']]
         self.assertEqual(result, expected, "Problem with test for preservation, case delete log")
 
         # Tests the contents of the case remains log.
@@ -171,8 +171,8 @@ class MyTestCase(unittest.TestCase):
                      'out_date', 'out_topic', 'out_text', 'out_document_name', 'out_fillin'],
                     ['Ms.', 'Gretel', 'G.', 'Green', 'nan', 'nan', 'nan', 'nan', '789 G St', 'nan', 'nan', 'nan',
                      'G city', 'GA', '78901', 'nan', 'g100', 'General', 'Email', '20210101', 'G1', 'nan',
-                     r'..\documents\BlobExport\objects\777777.txt', 'nan', 'r700', 'General', 'Email', '20210111',
-                     'nan', 'nan', r'..\documents\BlobExport\indivletters\000007.txt', 'Court case']]
+                     r'..\documents\objects\777777.txt', 'nan', 'r700', 'General', 'Email', '20210111',
+                     'nan', 'nan', r'..\documents\indivletters\000007.txt', 'Court case']]
         self.assertEqual(result, expected, "Problem with test for preservation, case remains log")
 
         # Tests the contents of the file deletion log.
@@ -180,19 +180,19 @@ class MyTestCase(unittest.TestCase):
         csv_path = os.path.join('test_data', 'script', f"file_deletion_log_{today}.csv")
         result = csv_to_list(csv_path)
         expected = [['File', 'SizeKB', 'DateCreated', 'DateDeleted', 'MD5', 'Notes'],
-                    [r'..\documents\BlobExport\objects\444444.txt'.replace('..', input_directory),
+                    [r'..\documents\objects\444444.txt'.replace('..', input_directory),
                      'nan', 'nan', 'nan', 'nan', 'Cannot delete: FileNotFoundError'],
-                    [r'..\documents\BlobExport\objects\555555.txt'.replace('..', input_directory),
+                    [r'..\documents\objects\555555.txt'.replace('..', input_directory),
                      'nan', 'nan', 'nan', 'nan', 'Cannot delete: FileNotFoundError'],
-                    [r'..\documents\BlobExport\objects\111111.txt'.replace('..', input_directory),
+                    [r'..\documents\objects\111111.txt'.replace('..', input_directory),
                      '0.0', today, today, '49C13D076A41E65DBE137D695E22A6A7', 'casework'],
-                    [r'..\documents\BlobExport\objects\333333.txt'.replace('..', input_directory),
+                    [r'..\documents\objects\333333.txt'.replace('..', input_directory),
                      'nan', 'nan', 'nan', 'nan', 'Cannot delete: FileNotFoundError'],
-                    [r'..\documents\BlobExport\indivletters\000005.txt'.replace('..', input_directory),
+                    [r'..\documents\indivletters\000005.txt'.replace('..', input_directory),
                      'nan', 'nan', 'nan', 'nan', 'Cannot delete: FileNotFoundError'],
-                    [r'..\documents\BlobExport\indivletters\000001.txt'.replace('..', input_directory),
+                    [r'..\documents\indivletters\000001.txt'.replace('..', input_directory),
                      '0.1', today, today, '21E65C7B733959A8B3E6071EB0748BF6', 'casework'],
-                    [r'..\documents\BlobExport\indivletters\000003.txt'.replace('..', input_directory),
+                    [r'..\documents\indivletters\000003.txt'.replace('..', input_directory),
                     '3.5', today, today, '3E273CCDD4D24DBFCD55B519999BABC7', 'casework']]
         self.assertEqual(result, expected, "Problem with test for preservation, file deletion log")
 
