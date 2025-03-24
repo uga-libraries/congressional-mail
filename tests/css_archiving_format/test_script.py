58f16aa7c064574dc31abbe94135ab5830e1938e
Adriane Hanson <ahansonuga@gmail.com>
2025-03-24 11:37:44 -0400

    Add script test for appraisal mode

--- tests/css_archiving_format/test_script.py
+++ tests/css_archiving_format/test_script.py
@@ -47,9 +47,11 @@ class MyTestCase(unittest.TestCase):
             if os.path.exists(file_path):
                 os.remove(file_path)
 
-        # Copy of input_directory made for the preservation test.
-        if os.path.exists(os.path.join('test_data', 'script', 'Preservation_Constituent_Mail_Export')):
-            shutil.rmtree(os.path.join('test_data', 'script', 'Preservation_Constituent_Mail_Export'))
+        # Copy of input_directory made for some test modes.
+        for mode in ('Accession', 'Appraisal', 'Preservation'):
+            copy_path = os.path.join('test_data', 'script', f'{mode}_Constituent_Mail_Export')
+            if os.path.exists(copy_path):
+                shutil.rmtree(copy_path)
 
     def test_correct_access(self):
         """Test for when the script runs correctly and is in access mode."""
@@ -152,6 +154,115 @@ class MyTestCase(unittest.TestCase):
                   os.path.exists(os.path.join(output_directory, 'topics_report.csv'))]
         expected = [False, False, False, False, False, False, False, False, False, False]
         self.assertEqual(result, expected, "Problem with test for access, other script mode outputs")
+
+    def test_correct_appraisal(self):
+        """Test for when the script runs correctly and is in appraisal mode."""
+        # Makes a copy of the test data in the repo, since the script alters the data.
+        shutil.copytree(os.path.join('test_data', 'script', 'Appraisal_Constituent_Mail_Export_copy'),
+                        os.path.join('test_data', 'script', 'Appraisal_Constituent_Mail_Export'))
+
+        # Runs the script.
+        script_path = os.path.join(os.getcwd(), '..', '..', 'css_archiving_format.py')
+        input_directory = os.path.join('test_data', 'script', 'Appraisal_Constituent_Mail_Export')
+        printed = subprocess.run(f"python {script_path} {input_directory} appraisal",
+                                 shell=True, capture_output=True, text=True)
+
+        # Tests the printed statement.
+        result = printed.stdout
+        expected = ("\nThe script is running in appraisal mode.\n"
+                    "It will delete letters due to appraisal but not change the metadata file.\n")
+        self.assertEqual(result, expected, "Problem with test for appraisal, printed statement")
+
+        # Tests the contents of the appraisal check log.
+        csv_path = os.path.join('test_data', 'script', 'appraisal_check_log.csv')
+        result = csv_to_list(csv_path)
+        expected = [['prefix', 'first', 'middle', 'last', 'suffix', 'appellation', 'title', 'org', 'addr1', 'addr2',
+                     'addr3', 'addr4', 'city', 'state', 'zip', 'country', 'in_id', 'in_type', 'in_method', 'in_date',
+                     'in_topic', 'in_text', 'in_document_name', 'in_fillin', 'out_id', 'out_type', 'out_method',
+                     'out_date', 'out_topic', 'out_text', 'out_document_name', 'out_fillin', 'Appraisal_Category'],
+                    ['Ms.', 'Gretel', 'G.', 'Green', 'nan', 'nan', 'nan', 'nan', '789 G St', 'nan', 'nan', 'nan',
+                     'G city', 'GA', '78901', 'nan', 'g100', 'General', 'Email', '20210101', 'E', 'nan',
+                     r'..\documents\BlobExport\objects\777777.txt', 'nan', 'r700', 'General', 'Email', '20210111',
+                     'nan', 'nan', r'..\documents\BlobExport\indivletters\000007.txt', 'Court case', 'Casework']]
+        self.assertEqual(result, expected, "Problem with test for appraisal, appraisal check log")
+
+        # Tests the contents of the appraisal delete log.
+        csv_path = os.path.join('test_data', 'script', 'appraisal_delete_log.csv')
+        result = csv_to_list(csv_path)
+        expected = [['prefix', 'first', 'middle', 'last', 'suffix', 'appellation', 'title', 'org', 'addr1', 'addr2',
+                     'addr3', 'addr4', 'city', 'state', 'zip', 'country', 'in_id', 'in_type', 'in_method', 'in_date',
+                     'in_topic', 'in_text', 'in_document_name', 'in_fillin', 'out_id', 'out_type', 'out_method',
+                     'out_date', 'out_topic', 'out_text', 'out_document_name', 'out_fillin', 'Appraisal_Category'],
+                    ['Mr.', 'Clive', 'C.', 'Cooper', 'Jr.', 'nan', 'CEO', 'Company', 'Attn: C', 'Division', 'POBox',
+                     '345 C St', 'C city', 'CO', '34567', 'nan', 'c300', 'General', 'Letter', '20240303', 'Misc',
+                     'Maybe casework', r'..\documents\BlobExport\objects\333333.txt', 'nan', 'r300', 'General',
+                     'Email', '2024-03-13', 'B1^B2', 'nan', r'..\documents\BlobExport\indivletters\000003.txt', 'nan',
+                     'Casework'],
+                    ['Ms.', 'Ann', 'A.', 'Anderson', 'nan', 'MD', 'nan', 'nan', '123 A St', 'nan', 'nan', 'nan',
+                     'A city', 'AL', '12345', 'nan', 'a100', 'General', 'Email', '20210101', 'Misc',
+                     'academy nomination',
+                     r'..\documents\BlobExport\objects\111111.txt', 'nan', 'r100', 'General', 'Email', '20210111',
+                     'nan', 'nan', r'..\documents\BlobExport\indivletters\000001.txt', 'nan', 'Academy_Application'],
+                    ['Ms.', 'Diane', 'D.', 'Dudly', 'nan', 'nan', 'nan', 'nan', '456 D St', 'nan', 'nan', 'nan',
+                     'D city', 'DEL', '45678', 'nan', 'd100', 'General', 'Email', '20210101', 'Casework', 'nan',
+                     r'..\documents\BlobExport\objects\444444.txt', 'nan', 'r400', 'General', 'Email', '20210111',
+                     'Recommendations', 'nan', r'..\documents\BlobExport\formletters\D.txt', 'nan',
+                     'Casework|Recommendation'],
+                    ['Ms.', 'Emma', 'E.', 'Evans', 'nan', 'nan', 'nan', 'nan', '567 E St', 'nan', 'nan', 'nan',
+                     'E city', 'ME', '56789', 'nan', 'e100', 'General', 'Email', '20210101', 'Recommendations', 'nan',
+                     r'..\documents\BlobExport\objects\555555.txt', 'nan', 'r500', 'General', 'Email', '20210111',
+                     'E', 'nan', r'..\documents\BlobExport\indivletters\000005.txt', 'nan', 'Recommendation'],
+                    ['Ms.', 'Fiona', 'F.', 'Fowler', 'nan', 'nan', 'nan', 'nan', '678 F St', 'nan', 'nan', 'nan',
+                     'F city', 'fl', '67890', 'nan', 'f100', 'General', 'Email', '20210101',
+                     'Social Security^Casework', 'nan', 'nan', 'nan', 'r600',
+                     'General', 'Email', '20210111', 'E', 'nan', r'..\documents\BlobExport\formletters\F.txt', 'nan',
+                     'Casework']]
+        self.assertEqual(result, expected, "Problem with test for appraisal, appraisal delete log")
+
+        # Tests the contents of the file deletion log.
+        today = date.today().strftime('%Y-%m-%d')
+        csv_path = os.path.join('test_data', 'script', f"file_deletion_log_{today}.csv")
+        result = csv_to_list(csv_path)
+        expected = [['File', 'SizeKB', 'DateCreated', 'DateDeleted', 'MD5', 'Notes'],
+                    [r'..\documents\objects\333333.txt'.replace('..', input_directory),
+                     '0.8', today, today, '09F83C9A1604F2DBCB8471DACCB99A49', 'Casework'],
+                    [r'..\documents\indivletters\000003.txt'.replace('..', input_directory),
+                     '3.5', today, today, '3E273CCDD4D24DBFCD55B519999BABC7', 'Casework'],
+                    [r'..\documents\objects\111111.txt'.replace('..', input_directory),
+                     '0.0', today, today, 'C53D3BB354B533DE159BB7C89E0433D5', 'Academy_Application'],
+                    [r'..\documents\indivletters\000001.txt'.replace('..', input_directory),
+                     '0.1', today, today, '21E65C7B733959A8B3E6071EB0748BF6', 'Academy_Application'],
+                    [r'..\documents\objects\444444.txt'.replace('..', input_directory),
+                     'nan', 'nan', 'nan', 'nan', 'Cannot delete: FileNotFoundError'],
+                    [r'..\documents\objects\555555.txt'.replace('..', input_directory),
+                     'nan', 'nan', 'nan', 'nan', 'Cannot delete: FileNotFoundError'],
+                    [r'..\documents\indivletters\000005.txt'.replace('..', input_directory),
+                     '1.2', today, today, 'EFBB58E35027F2382E2C58F22B895B7C', 'Recommendation']]
+        self.assertEqual(result, expected, "Problem with test for appraisal, file deletion log")
+
+        # Tests the contents of the input_directory, that all files that should be deleted are gone.
+        result = files_in_dir(input_directory)
+        expected = ['archiving_correspondence.dat', 'B.txt', 'D.txt', 'F.txt', '000007.txt',
+                    '222222.txt', '666666.txt', '777777.txt']
+        self.assertEqual(result, expected, "Problem with test for appraisal, input_directory contents")
+
+        # Tests the other script mode outputs were not made.
+        output_directory = os.path.join('test_data', 'script')
+        result = [os.path.exists(os.path.join(output_directory, 'archiving_correspondence_redacted.csv')),
+                  os.path.exists(os.path.join(output_directory, '2021-2022.csv')),
+                  os.path.exists(os.path.join(output_directory, '2023-2024.csv')),
+                  os.path.exists(os.path.join(output_directory, 'usability_report_metadata.csv')),
+                  os.path.exists(os.path.join(output_directory, 'metadata_formatting_errors_state.csv')),
+                  os.path.exists(os.path.join(output_directory, 'metadata_formatting_errors_zip.csv')),
+                  os.path.exists(os.path.join(output_directory, 'metadata_formatting_errors_in_date.csv')),
+                  os.path.exists(os.path.join(output_directory, 'metadata_formatting_errors_in_doc.csv')),
+                  os.path.exists(os.path.join(output_directory, 'metadata_formatting_errors_out_date.csv')),
+                  os.path.exists(os.path.join(output_directory, 'metadata_formatting_errors_out_doc.csv')),
+                  os.path.exists(os.path.join(output_directory, 'usability_report_matching.csv')),
+                  os.path.exists(os.path.join(output_directory, 'usability_report_matching_details.csv')),
+                  os.path.exists(os.path.join(output_directory, 'topics_report.csv'))]
+        expected = [False, False, False, False, False, False, False, False, False, False, False, False, False]
+        self.assertEqual(result, expected, "Problem with test for appraisal, other script mode outputs")
 
     def test_correct_preservation(self):
         """Test for when the script runs correctly and is in preservation mode."""
