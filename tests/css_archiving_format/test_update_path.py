cb062a0fd9fb541a60e14ece43a309b7778bb741
Adriane Hanson <ahansonuga@gmail.com>
2025-03-25 16:40:47 -0400

    Test for pattern BlobExport

---
+++ tests/css_archiving_format/test_update_path.py
@@ -0,0 +1,18 @@
+"""
+Tests for the function update_path(), which converts the path in the metadata to the file path in the export.
+"""
+import unittest
+from css_archiving_format import update_path
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_blobexport(self):
+        """Test for the pattern ..\\documents\\BlobExport\\folder\\..\\file.ext"""
+        file_path = update_path(r'..\documents\BlobExport\formletters\form_a.txt', 'input_dir')
+        expected = r'input_dir\documents\formletters\form_a.txt'
+        self.assertEqual(file_path, expected, "Problem with test for BlobExport")
+
+
+if __name__ == '__main__':
+    unittest.main()
