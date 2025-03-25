10cc4d5c44a8bd2bc61a412b7cfc95b863766ff3
Adriane Hanson <ahansonuga@gmail.com>
2025-03-25 16:52:11 -0400

    Update path tests (#44)
    * Test for pattern BlobExport
    
    * Test for dos
    
    * Test for new pattern
    
    * Delete unnecessary test files
    
    * Rename folder to out_document_name
    
    No longer need BlobExport and Dos variations of this, since calculating the path is a separate function.
    
    * Only one test for out_document_name

--- tests/css_archiving_format/test_delete_appraisal_letters.py
+++ tests/css_archiving_format/test_delete_appraisal_letters.py
@@ -20,8 +20,7 @@ class MyTestCase(unittest.TestCase):
 
         # Delete the copy of test data.
         test_dirs = [os.path.join('test_data', 'delete_appraisal_letters', 'in_document_name'),
-                     os.path.join('test_data', 'delete_appraisal_letters', 'out_document_name_blobexport'),
-                     os.path.join('test_data', 'delete_appraisal_letters', 'out_document_name_dos')]
+                     os.path.join('test_data', 'delete_appraisal_letters', 'out_document_name')]
         for test_dir in test_dirs:
             if os.path.exists(os.path.join(test_dir, 'Name_Constituent_Mail_Export')):
                 shutil.rmtree(os.path.join(test_dir, 'Name_Constituent_Mail_Export'))
@@ -67,41 +66,10 @@ class MyTestCase(unittest.TestCase):
         expected = ['form_a.txt', '333333.txt']
         self.assertEqual(result, expected, "Problem with test for in_document_name, directory contents")
 
-    def test_out_document_name_blobexport(self):
-        """Test for deleting files in the out_document_name column, for the path pattern including 'BlobExport'"""
+    def test_out_document_name(self):
+        """Test for deleting files in the out_document_name column"""
         # Makes a copy of the test data in the repo, since the script alters the data.
-        output_dir = os.path.join('test_data', 'delete_appraisal_letters', 'out_document_name_blobexport')
-        shutil.copytree(os.path.join(output_dir, 'Name_Constituent_Mail_Export_copy'),
-                        os.path.join(output_dir, 'Name_Constituent_Mail_Export'))
-
-        # Makes variables needed as function input and runs the function being tested.
-        input_dir = os.path.join(output_dir, 'Name_Constituent_Mail_Export')
-        appraisal_df = pd.DataFrame([['Anderson', '', r'..\documents\BlobExport\formletters\form_a.txt', 'Casework'],
-                                     ['Dudley', '', r'..\documents\BlobExport\indivletters\400.txt', 'Casework'],
-                                     ['Evans', '', r'..\documents\BlobExport\indivletters\500.txt', 'Casework']],
-                                    columns=['last', 'in_document_name', 'out_document_name', 'Appraisal_Category'])
-        delete_appraisal_letters(input_dir, appraisal_df)
-
-        # Tests the contents of the file deletion log.
-        today = date.today().strftime('%Y-%m-%d')
-        log_path = os.path.join(output_dir, f'file_deletion_log_{today}.csv')
-        result = csv_to_list(log_path)
-        expected = [['File', 'SizeKB', 'DateCreated', 'DateDeleted', 'MD5', 'Notes'],
-                    [r'..\documents\indivletters\400.txt'.replace('..', input_dir),
-                     2.4, today, today, '4CF163BB5919075DD6FEB7FC8D2AF3A8', 'Casework'],
-                    [r'..\documents\indivletters\500.txt'.replace('..', input_dir),
-                     1.8, today, today, '64ADE70B27A5D7923C5D3805B5671668', 'Casework'], ]
-        self.assertEqual(result, expected, "Problem with test for out_document_name blobexport, file deletion log")
-
-        # Tests the contents of the input_dir, that all files that should be deleted are gone.
-        result = files_in_dir(input_dir)
-        expected = ['form_a.txt', '100.txt']
-        self.assertEqual(result, expected, "Problem with test for out_document_name blobexport, directory contents")
-
-    def test_out_document_name_dos(self):
-        """Test for deleting files in the out_document_name column, for the path pattern including 'dos'"""
-        # Makes a copy of the test data in the repo, since the script alters the data.
-        output_dir = os.path.join('test_data', 'delete_appraisal_letters', 'out_document_name_dos')
+        output_dir = os.path.join('test_data', 'delete_appraisal_letters', 'out_document_name')
         shutil.copytree(os.path.join(output_dir, 'Name_Constituent_Mail_Export_copy'),
                         os.path.join(output_dir, 'Name_Constituent_Mail_Export'))
 
@@ -123,12 +91,12 @@ class MyTestCase(unittest.TestCase):
                      0.2, today, today, '45F12DDF78B657FA2DC1B0A2A0FB3ADD', 'Academy_Application'],
                     [r'..\documents\letter\333333.txt'.replace('..', input_dir),
                      0.7, today, today, '2CAA9E5BD685EFE4C9FCC9473375A86B', 'Casework'], ]
-        self.assertEqual(result, expected, "Problem with test for out_document_name dos, file deletion log")
+        self.assertEqual(result, expected, "Problem with test for out_document_name, file deletion log")
 
         # Tests the contents of the input_dir, that all files that should be deleted are gone.
         result = files_in_dir(input_dir)
         expected = ['form_a.txt', 'test.txt', '222222.txt']
-        self.assertEqual(result, expected, "Problem with test for out_document_name dos, directory contents")
+        self.assertEqual(result, expected, "Problem with test for out_document_name, directory contents")
 
     def test_error_new(self):
         """Test for when paths in the metadata do not match expected patterns"""
