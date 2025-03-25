04212881ce92cec6d390bc36f899df6234f66923
Adriane Hanson <ahansonuga@gmail.com>
2025-03-25 16:42:59 -0400

    Test for dos

--- tests/css_archiving_format/test_update_path.py
+++ tests/css_archiving_format/test_update_path.py
@@ -13,6 +13,12 @@ class MyTestCase(unittest.TestCase):
         expected = r'input_dir\documents\formletters\form_a.txt'
         self.assertEqual(file_path, expected, "Problem with test for BlobExport")
 
+    def test_dos(self):
+        """Test for the pattern \\\\name-office\\dos\\public\\folder\\..\\file.ext"""
+        file_path = update_path(r'\\office-dc\dos\public\letter\111111.txt', 'input_dir')
+        expected = r'input_dir\documents\letter\111111.txt'
+        self.assertEqual(file_path, expected, "Problem with test for Dos")
+
 
 if __name__ == '__main__':
     unittest.main()
