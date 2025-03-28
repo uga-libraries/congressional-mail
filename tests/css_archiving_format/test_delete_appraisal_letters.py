019565100a33de734f1a7aa9278fa68d4f76eef9
Adriane Hanson <ahansonuga@gmail.com>
2025-03-28 10:43:27 -0400

    Add tests for in_document_name rows to skip

--- tests/css_archiving_format/test_delete_appraisal_letters.py
+++ tests/css_archiving_format/test_delete_appraisal_letters.py
@@ -45,8 +45,8 @@ class MyTestCase(unittest.TestCase):
         input_dir = os.path.join(output_dir, 'Name_Constituent_Mail_Export')
         appraisal_df = pd.DataFrame([['Anderson', r'..\documents\BlobExport\objects\111111.txt',
                                       r'..\documents\BlobExport\formletters\form_a.txt', 'Academy_Application'],
-                                     ['Blue', r'..\documents\BlobExport\objects\222222.txt', '', 'Academy_Application'],
-                                     ['Cooper', '', r'..\documents\BlobExport\formletters\test.txt', 'Casework']],
+                                     ['Blue', r'..\documents\BlobExport\objects\222222.txt', '',
+                                      'Academy_Application']],
                                     columns=['last', 'in_document_name', 'out_document_name', 'Appraisal_Category'])
         delete_appraisal_letters(input_dir, appraisal_df)
 
@@ -65,6 +65,38 @@ class MyTestCase(unittest.TestCase):
         result = files_in_dir(input_dir)
         expected = ['form_a.txt', '333333.txt']
         self.assertEqual(result, expected, "Problem with test for in_document_name, directory contents")
+
+    def test_in_skip(self):
+        """Test for deleting files in the in_document_name column, where some rows should be skipped"""
+        # Makes a copy of the test data in the repo, since the script alters the data.
+        output_dir = os.path.join('test_data', 'delete_appraisal_letters', 'in_document_name')
+        shutil.copytree(os.path.join(output_dir, 'Name_Constituent_Mail_Export_copy'),
+                        os.path.join(output_dir, 'Name_Constituent_Mail_Export'))
+
+        # Makes variables needed as function input and runs the function being tested.
+        input_dir = os.path.join(output_dir, 'Name_Constituent_Mail_Export')
+        appraisal_df = pd.DataFrame([['Abe', r'..\documents\BlobExport\objects\111111.txt', '', 'Academy_Application'],
+                                     ['Blue', r'..\documents\BlobExport\objects\222222.txt', '', 'Academy_Application'],
+                                     ['Cooper', '', r'..\documents\BlobExport\formletters\test.txt', 'Casework'],
+                                     ['Dane', 'nan', r'..\documents\BlobExport\formletters\test.txt', 'Casework']],
+                                    columns=['last', 'in_document_name', 'out_document_name', 'Appraisal_Category'])
+        delete_appraisal_letters(input_dir, appraisal_df)
+
+        # Tests the contents of the file deletion log.
+        today = date.today().strftime('%Y-%m-%d')
+        log_path = os.path.join(output_dir, f'file_deletion_log_{today}.csv')
+        result = csv_to_list(log_path)
+        expected = [['File', 'SizeKB', 'DateCreated', 'DateDeleted', 'MD5', 'Notes'],
+                    [r'..\documents\objects\111111.txt'.replace('..', input_dir),
+                     0.2, today, today, '45F12DDF78B657FA2DC1B0A2A0FB3ADD', 'Academy_Application'],
+                    [r'..\documents\objects\222222.txt'.replace('..', input_dir),
+                     0.7, today, today, '2CAA9E5BD685EFE4C9FCC9473375A86B', 'Academy_Application'], ]
+        self.assertEqual(result, expected, "Problem with test for in_skip, file deletion log")
+
+        # Tests the contents of the input_dir, that all files that should be deleted are gone.
+        result = files_in_dir(input_dir)
+        expected = ['form_a.txt', '333333.txt']
+        self.assertEqual(result, expected, "Problem with test for in_skip, directory contents")
 
     def test_out_document_name(self):
         """Test for deleting files in the out_document_name column"""
