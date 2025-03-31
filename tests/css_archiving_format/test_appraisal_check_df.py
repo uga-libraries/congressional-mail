1036ebc009e37f411651f8c94830bff67c28925a
Adriane Hanson <ahansonuga@gmail.com>
2025-03-31 16:23:05 -0400

    Add test for no columns

--- tests/css_archiving_format/test_appraisal_check_df.py
+++ tests/css_archiving_format/test_appraisal_check_df.py
@@ -54,6 +54,21 @@ class MyTestCase(unittest.TestCase):
                     ['30606', '', '', r'doc\file4.txt', 'Note', r'letter\123.txt', 'Mr._Case', 'Casework']]
         self.assertEqual(result, expected, "Problem with test for one column")
 
+    def test_no_column(self):
+        """Test for when the keyword is in no columns"""
+        # Makes a dataframe to use as test input and runs the function.
+        df = pd.DataFrame([['30600', 'note', r'doc\file1.txt', '', '', r'form\filea.txt', 'a_b'],
+                           ['30602', 'note', '', '', '', r'form\a.txt', ''],
+                           ['30603', '', '', '', 'started Tue', '', 'a_b']],
+                          columns=['zip', 'in_text', 'in_document_name', 'in_fillin',
+                                   'out_text', 'out_document_name', 'out_fillin'])
+        df_check = appraisal_check_df(df, 'case', 'Casework')
+
+        # Tests the values in df_check are correct.
+        result = df_to_list(df_check)
+        expected = [['zip', 'in_text', 'in_document_name', 'in_fillin', 'out_text', 'out_document_name', 'out_fillin',
+                     'Appraisal_Category']]
+        self.assertEqual(result, expected, "Problem with test for no column")
 
 if __name__ == '__main__':
     unittest.main()
