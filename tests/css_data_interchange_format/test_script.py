73b44fc6869621c1f02f38951d209f9afbf51133
Adriane Hanson <ahansonuga@gmail.com>
2025-04-25 16:50:36 -0400

    Add script test in accession mode

--- tests/css_data_interchange_format/test_script.py
+++ tests/css_data_interchange_format/test_script.py
@@ -31,9 +31,10 @@ class MyTestCase(unittest.TestCase):
     def tearDown(self):
         """Remove script outputs, if they were made"""
         # Metadata file and logs in the input directory.
-        filenames = ['appraisal_check_log.csv', 'appraisal_delete_log.csv',
+        filenames = ['appraisal_check_log.csv', 'appraisal_delete_log.csv', 'archiving_correspondence_redacted.csv'
                      f"file_deletion_log_{date.today().strftime('%Y-%m-%d')}.csv",
-                     'archiving_correspondence_redacted.csv']
+                     'topics_report.csv', 'usability_report_matching.csv', 'usability_report_matching_details.csv',
+                     'usability_report_metadata.csv']
         for filename in filenames:
             file_path = os.path.join('test_data', 'script', filename)
             if os.path.exists(file_path):
@@ -43,6 +44,10 @@ class MyTestCase(unittest.TestCase):
         file_path = os.path.join('test_data', 'script', 'archiving_correspondence_by_congress_year')
         if os.path.exists(file_path):
             shutil.rmtree(file_path)
+
+        # Metadata file saving to wrong folder - will fix later.
+        if os.path.exists(os.path.join('test_data', 'metadata_formatting_errors_state_code.csv')):
+            os.remove(os.path.join('test_data', 'metadata_formatting_errors_state_code.csv'))
 
         # Copy of test data.
         test_folders = ['access_test', 'appraisal_test']
@@ -170,6 +175,133 @@ class MyTestCase(unittest.TestCase):
                      'nan', 'OUTGOING', r'..\documents\formletters\Airline Passenger BOR Act2 1999.doc',
                      'Airline Passenger BOR Act2 1999', ' ', 'nan']]
         self.assertEqual(result, expected, "Problem with test for access, undated.csv")
+
+    def test_accession(self):
+        """Test for when the script runs correctly in accession mode."""
+        # Runs the script.
+        script_path = os.path.join(os.getcwd(), '..', '..', 'css_data_interchange_format.py')
+        input_directory = os.path.join('test_data', 'script', 'accession_test')
+        output = subprocess.run(f"python {script_path} {input_directory} accession",
+                                shell=True, capture_output=True, text=True)
+
+        # Tests the print statement.
+        result = output.stdout
+        expected = ('\nThe script is running in accession mode.\n'
+                    'It will produce usability and appraisal reports and not change the export.\n')
+        self.assertEqual(result, expected, "Problem with test for accession, printed statement")
+
+        # Tests the contents of the appraisal_check_log.csv.
+        csv_path = os.path.join('test_data', 'script', 'appraisal_check_log.csv')
+        result = csv_to_list(csv_path)
+        expected = [['city', 'state_code', 'zip_code', 'country', 'communication_type', 'approved_by', 'status',
+                     'date_in', 'date_out', 'reminder_date', 'update_date', 'response_type', 'group_name',
+                     'document_type', 'communication_document_name', 'communication_document_id', 'file_location',
+                     'file_name', 'Appraisal_Category'],
+                    ['Washington', 'DC', '20420-0002', 'USA', 'nan', '513', 'C', '19990721', '19990721', 'nan',
+                     '19990721', 'imail', 'nan', 'OUTGOING', r'..\documents\formletters\legal_case.html',
+                     'legal_case.html', ' ', 'nan', 'Casework']]
+        self.assertEqual(result, expected, "Problem with test for accession, appraisal_check_log.csv")
+
+        # Tests the contents of the appraisal_delete_log.csv.
+        csv_path = os.path.join('test_data', 'script', 'appraisal_delete_log.csv')
+        result = csv_to_list(csv_path)
+        expected = [['city', 'state_code', 'zip_code', 'country', 'communication_type', 'approved_by', 'status',
+                     'date_in', 'date_out', 'reminder_date', 'update_date', 'response_type', 'group_name',
+                     'document_type', 'communication_document_name', 'communication_document_id', 'file_location',
+                     'file_name', 'Appraisal_Category'],
+                    [' ', ' ', 'nan', 'POLAND', 'usmail', 'nan', 'C', '19990331', '19990402', 'nan', '19990331',
+                     'usmail', 'CASE 1', 'OUTGOING', r'..\documents\indivletters\00001.doc', '00001.doc',
+                     ' ', 'nan', 'Casework'],
+                    ['Ellijay', 'GA', '30540', 'USA', 'usmail', 'nan', 'C', '20000427', '20000427', 'nan', '20000427',
+                     'usmail', 'CASE2', 'OUTGOING', r'..\documents\indivletters\00002.doc', '00002.doc', ' ', 'nan',
+                     'Casework'],
+                    ['Marietta', 'GA', '30062-1668', 'USA', 'nan', '513', 'C', '20120914', '20120914', 'nan',
+                     '20120914', 'imail', 'CASE 3', 'OUTGOING', r'..\documents\formletters\2103422.html',
+                     '2103422.html', ' ', 'nan', 'Casework'],
+                    ['Marietta', 'GA', '30062-1668', 'USA', 'nan', '551', 'C', '19990315', '19990402', 'nan',
+                     '19990315', 'imail', 'CASE4', 'INCOMING', r'..\documents\objects\4007000.eml', 'nan',
+                     '1c8614bf01caf83e00010e44.eml', 'nan', 'Casework'],
+                    ['Marietta', 'GA', '30067-8581', 'USA', 'nan', '513', 'C', '20000427', '20000427', 'nan',
+                     '20000427', 'imail', 'nan', 'OUTGOING', r'..\documents\indivletters\casework_12345.doc',
+                     'nan', ' ', 'nan', 'Casework']]
+        self.assertEqual(result, expected, "Problem with test for accession, appraisal_delete_log.csv")
+
+        # # Tests the contents of the metadata_1.csv.
+        # csv_path = os.path.join('test_data', 'script', 'name.csv')
+        # result = csv_to_list(csv_path)
+        # expected = []
+        # self.assertEqual(result, expected, "Problem with test for accession, name.csv")
+        #
+        # Tests the contents of the metadata_formatting_errors_state_code.csv.
+        csv_path = os.path.join('test_data', 'metadata_formatting_errors_state_code.csv')
+        result = csv_to_list(csv_path)
+        expected = [['city', 'state_code', 'zip_code', 'country', 'communication_type', 'approved_by', 'status',
+                     'date_in', 'date_out', 'reminder_date', 'update_date', 'response_type', 'group_name',
+                     'document_type', 'communication_document_name', 'communication_document_id', 'file_location',
+                     'file_name'],
+                    [' ', ' ', 'nan', 'POLAND', 'usmail', 'nan', 'C', '19990331', '19990402', 'nan', '19990331',
+                     'usmail', 'CASE 1', 'OUTGOING', r'..\documents\indivletters\00001.doc', '00001.doc', ' ', 'nan'],
+                    [' ', ' ', 'nan', 'POLAND', 'usmail', 'nan', 'C', '19990315', '19990402', 'nan', '19990315',
+                     'usmail', 'INTTAX', 'OUTGOING', r'..\documents\formletters\busintax.doc', 'busintax.doc', ' ',
+                     'nan']]
+        self.assertEqual(result, expected, "Problem with test for accession, metadata_formatting_errors_state_code.csv")
+
+        # Tests the contents of the topics_report.csv.
+        csv_path = os.path.join('test_data', 'script', 'topics_report.csv')
+        result = csv_to_list(csv_path)
+        expected = [['Topic', 'Topic_Count'],
+                    ['BLANK', '4'],
+                    ['CASE2', '1'],
+                    ['CASE 1', '1'],
+                    ['INTTAX', '1'],
+                    ['CASE 3', '1'],
+                    ['CASE4', '1']]
+        self.assertEqual(result, expected, "Problem with test for accession, topics_report.csv")
+
+        # Tests the contents of the usability_report_matching.csv.
+        csv_path = os.path.join('test_data', 'script', 'usability_report_matching.csv')
+        result = csv_to_list(csv_path)
+        expected = [['Category', 'Count'],
+                    ['Metadata_Only', '3'],
+                    ['Directory_Only', '1'],
+                    ['Match', '6'],
+                    ['Metadata_Blank', '0']]
+        self.assertEqual(result, expected, "Problem with test for accession, usability_report_matching.csv")
+
+        # Tests the contents of the usability_report_matching_details.csv.
+        csv_path = os.path.join('test_data', 'script', 'usability_report_matching_details.csv')
+        result = csv_to_list(csv_path)
+        result.sort()
+        expected = [['Category', 'Path'],
+                    ['Directory Only', r'test_data\script\accession_test\documents\indivletters\casework_999999.doc'],
+                    ['Metadata Only', r'test_data\script\accession_test\documents\formletters\Airline Act2.doc'],
+                    ['Metadata Only', r'test_data\script\accession_test\documents\formletters\busintax.doc'],
+                    ['Metadata Only', r'test_data\script\accession_test\documents\indivletters\00002.doc']]
+        self.assertEqual(result, expected, "Problem with test for accession, usability_report_matching_details.csv")
+
+        # Tests the contents of the usability_report_metadata.csv.
+        csv_path = os.path.join('test_data', 'script', 'usability_report_metadata.csv')
+        result = csv_to_list(csv_path)
+        expected = [['Column_Name', 'Present', 'Blank_Count', 'Blank_Percent', 'Formatting_Errors'],
+                    ['city', 'True', '0', '0.0', 'uncheckable'],
+                    ['state_code', 'True', '0', '0.0', '2'],
+                    ['zip_code', 'True', '2', '22.22', '0'],
+                    ['country', 'True', '0', '0.0', 'uncheckable'],
+                    ['communication_type', 'True', '4', '44.44', 'uncheckable'],
+                    ['approved_by', 'True', '5', '55.56', 'uncheckable'],
+                    ['status', 'True', '0', '0.0', 'uncheckable'],
+                    ['date_in', 'True', '2', '22.22', '0'],
+                    ['date_out', 'True', '2', '22.22', '0'],
+                    ['reminder_date', 'True', '9', '100.0', '0'],
+                    ['update_date', 'True', '2', '22.22', '0'],
+                    ['response_type', 'True', '0', '0.0', 'uncheckable'],
+                    ['group_name', 'True', '4', '44.44', 'uncheckable'],
+                    ['document_type', 'True', '0', '0.0', 'uncheckable'],
+                    ['communication_document_name', 'True', '0', '0.0', '0'],
+                    ['communication_document_id', 'True', '2', '22.22', 'uncheckable'],
+                    ['file_location', 'True', '0', '0.0', 'uncheckable'],
+                    ['file_name', 'True', '9', '100.0', 'uncheckable']]
+        self.assertEqual(result, expected, "Problem with test for accession, usability_report_metadata.csv")
 
     def test_appraisal(self):
         """Test for when the script runs correctly in appraisal mode."""
