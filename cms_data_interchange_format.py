1a57efb5bfd3962222129a5fb2ca52545ae8f41e
Adriane M Hanson <ahansonuga@gmail.com>
2025-05-12 15:52:34 -0400

    Create check_metadata_formatting()

--- cms_data_interchange_format.py
+++ cms_data_interchange_format.py
@@ -75,6 +75,38 @@ def check_arguments(arg_list):
     return input_dir, md_paths, mode, errors
 
 
+def check_metadata_formatting(column, df, output_dir):
+    """Return the number of rows that don't meet the expected formatting and save the rows to a csv"""
+
+    # Dictionary of expected formatting patterns for each column.
+    patterns = {'correspondence_document_name': r'^attachments|^case-custom|^case-files|^documents|^enewsletters|'
+                                                r'^form-attachments|^forms|^in-email|^out-custom',
+                'date_in': r'^\d{8}$',
+                'date_out': r'^\d{8}$',
+                'state': r'^[A-Z][A-Z]$',
+                'tickler_date': r'^\d{8}$',
+                'update_date': r'^\d{8}$',
+                'zip_code': r'^\d{5}(-\d{4})?$'}
+
+    # Makes a dataframe with all rows that do not match the expected formatting, excluding blanks.
+    # If the column is missing from the dataframe or blank, it returns default text instead of a row count.
+    try:
+        match = df[column].str.contains(patterns[column], regex=True, na=False)
+    except KeyError:
+        return 'column_missing'
+    except AttributeError:
+        return 'column_blank'
+    df_no_match = df[~match & df[column].notna()]
+
+    # Saves the dataframe to a csv if there were any that did not match the expected formatting.
+    no_match_count = len(df_no_match.index)
+    if no_match_count > 0:
+        df_no_match.to_csv(os.path.join(output_dir, f'metadata_formatting_errors_{column}.csv'), index=False)
+
+    # Returns the number of rows that do not match the expected formatting, excluding blanks.
+    return no_match_count
+
+
 def delete_appraisal_letters(input_dir, output_dir, df_appraisal):
     """Deletes letters received from constituents and individual letters sent back by the office
     because they are one of the types of letters not retained for appraisal reasons"""
