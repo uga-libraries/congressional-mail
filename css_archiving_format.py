eec334b872b586145b65e46e2d7c912b14b4f126
Adriane M Hanson <ahansonuga@gmail.com>
2025-07-29 16:58:21 -0400

    Blanks are string 'nan' because read all as string

--- css_archiving_format.py
+++ css_archiving_format.py
@@ -560,17 +560,20 @@ def sort_correspondence(df, input_dir, output_dir):
     # Makes a dataframe with any row that has values in in_topic and in_document_name,
     # with rows split and in_document_name repeated if there is more than one in_topic (divided by ^)
     # and any duplicate combinations of in_topic and in_document_name removed.
-    sort_df = df[(df['in_topic'] != 'BLANK') & (df['in_document_name'].notna()) & (df['in_document_name'] != '')]
+    sort_df = df[(df['in_topic'] != 'nan') & (df['in_document_name'] != 'nan')]
     sort_df['in_topic'] = sort_df['in_topic'].str.split(r'^')
     sort_df = sort_df.explode('in_topic')
     sort_df = sort_df.drop_duplicates(subset=['in_topic', 'in_document_name'])
+    print("\nRows in sort_df", len(sort_df.index))
 
     # For each topic in in_topic, makes a folder in the output directory with that topic
     # and copies all documents with that topic into the folder, updating the metadata path to match the directory.
     os.mkdir(os.path.join(output_dir, 'Correspondence_by_Topic'))
     topic_list = sort_df['in_topic'].unique()
     for topic in topic_list:
+        print("\nStarting on topic", topic)
         doc_list = sort_df.loc[sort_df['in_topic'] == topic, 'in_document_name'].tolist()
+        print("Doc_list", doc_list)
         # Characters that Windows does not permit in a folder name are replaced with an underscore.
         for character in ('\\', '/', ':', '*', '?', '"', '<', '>', '|'):
             topic = topic.replace(character, '_')
