8cc49689db85d02fdf6905103f814870664fbc9e
Adriane M Hanson <ahansonuga@gmail.com>
2025-08-01 16:34:46 -0400

    Update script test

--- tests/cms_data_interchange_format/test_script.py
+++ tests/cms_data_interchange_format/test_script.py
@@ -7,6 +7,7 @@ import pandas as pd
 import shutil
 import subprocess
 import unittest
+from test_sort_correspondence import make_dir_list
 
 
 def csv_to_list(csv_path, sort=False):
@@ -49,6 +50,11 @@ class MyTestCase(unittest.TestCase):
         if os.path.exists(folder_path):
             shutil.rmtree(folder_path)
 
+        # Incoming letters organized by topic, in own directory.
+        file_path = os.path.join('test_data', 'script', 'Correspondence_by_Topic')
+        if os.path.exists(file_path):
+            shutil.rmtree(file_path)
+
         # Copy of test data for appraisal mode, which is altered by the script (files are deleted)).
         copy_path = os.path.join('test_data', 'script', 'appraisal_copy')
         if os.path.exists(copy_path):
@@ -64,9 +70,9 @@ class MyTestCase(unittest.TestCase):
 
         # Tests the print statement.
         result = output.stdout
-        expected = ('\nThe script is running in access mode.\nIt will remove rows for deleted letters, '
-                    'save the merged metadata tables without columns with PII,'
-                    ' and make copies of the metadata split by congress year\n')
+        expected = ('\nThe script is running in access mode.\nIt will remove rows for deleted letters '
+                    'and columns with PII, make copies of the metadata split by congress year, '
+                    'and make a copy of the constituent letters organized by topic\n')
         self.assertEqual(result, expected, "Problem with test for access, printed statement")
 
         # Tests the contents of the appraisal_check_log.csv.
@@ -78,7 +84,7 @@ class MyTestCase(unittest.TestCase):
                      'correspondence_text', 'code_type', 'code', 'code_description', 'inactive_flag',
                      'Appraisal_Category'],
                     ['City One', 'GA', '30001', 'USA', 'LETTER', 'Staffer_1', '20210110', '20210110', 'BLANK',
-                     '20210110', 'LETTER', '11111', 'CON', '1', 'main', 'legal_con.docx', 'BLANK', 'note text 1',
+                     '20210110', 'LETTER', '11111', 'CON', '1', 'main', r'in-email\1.txt', 'BLANK', 'note text 1',
                      'COR', '11111', 'LEGAL CASE', 'Y', 'Casework']]
         self.assertEqual(result, expected, "Problem with test for access, appraisal_check_log.csv")
 
@@ -100,19 +106,19 @@ class MyTestCase(unittest.TestCase):
                      '2C_sequence_number', 'document_type', 'correspondence_document_name', 'file_location',
                      'code_type', 'code', 'code_description', 'inactive_flag'],
                     ['City One', 'GA', '30001', 'USA', 'LETTER', 'Staffer_1', '20210110', '20210110', 'BLANK',
-                     '20210110', 'LETTER', '11111', 'CON', '1', 'main', 'legal_con.docx', 'BLANK',
+                     '20210110', 'LETTER', '11111', 'CON', '1', 'main', r'in-email\1.txt', 'BLANK',
                      'COR', '11111', 'LEGAL CASE', 'Y'],
                     ['Caseyville', 'GA', '30002', 'USA', 'EMAIL', 'Staffer_2', '20220220', '20220220', 'BLANK',
-                     '20220220', 'EMAIL', '22222', 'PRO', '1', 'main', 'min_wage_pro.docx', 'BLANK',
+                     '20220220', 'EMAIL', '22222', 'PRO', '1', 'main', r'in-email\2.txt', 'BLANK',
                      'COR', '22222', 'MINWAGE', 'Y'],
                     ['City Three', 'GA', '30003', 'USA', 'EMAIL', 'Staffer_3', '20220330', '20220330', 'BLANK',
-                     '20220330', 'EMAIL', '33333', 'PRO', '1', 'main', 'rights_pro.docx', 'BLANK',
+                     '20220330', 'EMAIL', '33333', 'PRO', '1', 'main', r'in-email\3.txt', 'BLANK',
                      'COR', '33333', 'RIGHTS', 'Y'],
                     ['City One', 'GA', '30001', 'USA', 'EMAIL', 'Staffer_3', 'BLANK', 'BLANK', 'BLANK',
-                     'BLANK', 'EMAIL', '33333', 'PRO', '1', 'main', 'rights_pro.docx', 'BLANK',
+                     'BLANK', 'EMAIL', '33333', 'PRO', '1', 'main', r'in-email\33.txt', 'BLANK',
                      'COR', '33333', 'RIGHTS', 'Y'],
                     ['City One', 'GA', '30001', 'USA', 'EMAIL', 'Staffer_3', '20230330', '20230330', 'BLANK',
-                     '20230330', 'EMAIL', '33333', 'PRO', '1', 'main', 'rights_pro.docx', 'BLANK',
+                     '20230330', 'EMAIL', '33333', 'PRO', '1', 'main', r'in-email\333.txt', 'BLANK',
                      'COR', '33333', 'RIGHTS', 'Y']]
         self.assertEqual(result, expected, "Problem with test for access, archiving_correspondence_redacted.csv")
 
@@ -124,13 +130,13 @@ class MyTestCase(unittest.TestCase):
                      '2C_sequence_number', 'document_type', 'correspondence_document_name', 'file_location',
                      'code_type', 'code', 'code_description', 'inactive_flag'],
                     ['City One', 'GA', '30001', 'USA', 'LETTER', 'Staffer_1', '20210110', '20210110', 'BLANK',
-                     '20210110', 'LETTER', '11111', 'CON', '1', 'main', 'legal_con.docx', 'BLANK',
+                     '20210110', 'LETTER', '11111', 'CON', '1', 'main', r'in-email\1.txt', 'BLANK',
                      'COR', '11111', 'LEGAL CASE', 'Y'],
                     ['Caseyville', 'GA', '30002', 'USA', 'EMAIL', 'Staffer_2', '20220220', '20220220', 'BLANK',
-                     '20220220', 'EMAIL', '22222', 'PRO', '1', 'main', 'min_wage_pro.docx', 'BLANK',
+                     '20220220', 'EMAIL', '22222', 'PRO', '1', 'main', r'in-email\2.txt', 'BLANK',
                      'COR', '22222', 'MINWAGE', 'Y'],
                     ['City Three', 'GA', '30003', 'USA', 'EMAIL', 'Staffer_3', '20220330', '20220330', 'BLANK',
-                     '20220330', 'EMAIL', '33333', 'PRO', '1', 'main', 'rights_pro.docx', 'BLANK',
+                     '20220330', 'EMAIL', '33333', 'PRO', '1', 'main', r'in-email\3.txt', 'BLANK',
                      'COR', '33333', 'RIGHTS', 'Y']]
         self.assertEqual(result, expected, "Problem with test for access, 2021-2022")
 
@@ -142,7 +148,7 @@ class MyTestCase(unittest.TestCase):
                      '2C_sequence_number', 'document_type', 'correspondence_document_name', 'file_location',
                      'code_type', 'code', 'code_description', 'inactive_flag'],
                     ['City One', 'GA', '30001', 'USA', 'EMAIL', 'Staffer_3', '20230330', '20230330', 'BLANK',
-                     '20230330', 'EMAIL', '33333', 'PRO', '1', 'main', 'rights_pro.docx', 'BLANK',
+                     '20230330', 'EMAIL', '33333', 'PRO', '1', 'main', r'in-email\333.txt', 'BLANK',
                      'COR', '33333', 'RIGHTS', 'Y']]
         self.assertEqual(result, expected, "Problem with test for access, 2023-2024")
 
@@ -154,9 +160,19 @@ class MyTestCase(unittest.TestCase):
                      '2C_sequence_number', 'document_type', 'correspondence_document_name', 'file_location',
                      'code_type', 'code', 'code_description', 'inactive_flag'],
                     ['City One', 'GA', '30001', 'USA', 'EMAIL', 'Staffer_3', 'BLANK', 'BLANK', 'BLANK',
-                     'BLANK', 'EMAIL', '33333', 'PRO', '1', 'main', 'rights_pro.docx', 'BLANK',
+                     'BLANK', 'EMAIL', '33333', 'PRO', '1', 'main', r'in-email\33.txt', 'BLANK',
                      'COR', '33333', 'RIGHTS', 'Y']]
         self.assertEqual(result, expected, "Problem with test for access, undated")
+
+        # Tests that Correspondence_by_Topic has the expected files.
+        by_topic = os.path.join(os.getcwd(), 'test_data', 'script', 'Correspondence_by_Topic')
+        result = make_dir_list(by_topic)
+        expected = [os.path.join(by_topic, 'LEGAL CASE', '1.txt'),
+                    os.path.join(by_topic, 'MINWAGE', '2.txt'),
+                    os.path.join(by_topic, 'RIGHTS', '3.txt'),
+                    os.path.join(by_topic, 'RIGHTS', '33.txt'),
+                    os.path.join(by_topic, 'RIGHTS', '333.txt')]
+        self.assertEqual(result, expected, "Problem with test for access, Correspondence_by_Topic")
 
     def test_accession(self):
         """Test for when the script runs correctly in accession mode"""
