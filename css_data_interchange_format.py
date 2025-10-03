547b8e4c70737580c012033f52ee86820864398d
Adriane M Hanson <ahansonuga@gmail.com>
2025-10-03 14:32:34 -0400

    css_dif lowercase letter matching

--- css_data_interchange_format.py
+++ css_data_interchange_format.py
@@ -84,7 +84,8 @@ def check_arguments(arg_list):
 
 
 def check_letter_matching(df, output_dir, input_dir):
-    """Compare the files in the metadata to the files in the export"""
+    """Compare the files in the metadata to the files in the export,
+    reformatting the metadata paths and making all characters lowercase, so they can match"""
 
     # Makes a list of paths for the letters in the documents folder within the input directory.
     # This way, the metadata files are not counted as missing.
@@ -92,11 +93,13 @@ def check_letter_matching(df, output_dir, input_dir):
     for root, dirs, files in os.walk(os.path.join(input_dir, 'documents')):
         for file in files:
             file_path = os.path.join(root, file)
+            file_path = file_path.lower()
             input_dir_paths.append(file_path)
 
     # Makes a list of paths in the metadata, updating the path to match how the directory is structured in the export.
     doc_df = df.dropna(subset=['communication_document_name']).copy()
     doc_df['communication_document_name'] = doc_df['communication_document_name'].apply(update_path, input_dir=input_dir)
+    doc_df['communication_document_name'] = doc_df['communication_document_name'].str.lower()
     metadata_paths = doc_df['communication_document_name'].tolist()
 
     # Number of metadata rows without a file path.
