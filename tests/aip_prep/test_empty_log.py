b4dff38ac4b4898171b3ccc3c51ac5afc00a8ca3
Adriane M Hanson <ahansonuga@gmail.com>
2026-01-06 14:08:28 -0500

    Create test_empty_log.py

---
+++ tests/aip_prep/test_empty_log.py
@@ -0,0 +1,50 @@
+import datetime
+import os
+import unittest
+from aip_prep import empty_log
+from test_script import text_to_list
+
+
+class MyTestCase(unittest.TestCase):
+
+    def tearDown(self):
+        """Deletes the temporary folder which would contain the log, if made"""
+        temp = os.path.join(os.getcwd(), 'empty_log_test')
+        if os.path.exists(temp):
+            os.remove(temp)
+
+    def test_new(self):
+        """Test for making the log for the first time"""
+        # Makes variables needed as function input and runs the function.
+        output_dir = os.path.join(os.getcwd(), 'empty_log_test')
+        os.mkdir(output_dir)
+        empty_log(output_dir, 'C:/test/folder_empty')
+
+        # Tests the contents of the empty subfolder log.
+        result = text_to_list(os.path.join(output_dir, 'empty_subfolders_log.txt'))
+        today = datetime.datetime.now().strftime('%Y-%m-%d')
+        expected = [f"The following folders were empty on {today} "
+                    f"when this export was split into smaller folders for AIP creation:\r\n", '\r\n',
+                    "C:/test/folder_empty\r\n"]
+        self.assertEqual(expected, result, "Problem with test for new log")
+
+    def test_addition(self):
+        """Test for adding a row to an existing log"""
+        # Makes variables needed as function input and runs the function twice.
+        output_dir = os.path.join(os.getcwd(), 'empty_log_test')
+        os.mkdir(output_dir)
+        empty_log(output_dir, 'C:/test/empty_one')
+        empty_log(output_dir, 'C:/test/empty_two')
+
+        # Tests the contents of the empty subfolder log.
+        result = text_to_list(os.path.join(output_dir, 'empty_subfolders_log.txt'))
+        today = datetime.datetime.now().strftime('%Y-%m-%d')
+        expected = [f"The following folders were empty on {today} "
+                    f"when this export was split into smaller folders for AIP creation:\r\n", '\r\n',
+                    "C:/test/empty_one\r\n",
+                    "C:/test/empty_two\r\n"]
+        self.assertEqual(expected, result, "Problem with test for addition to log")
+
+
+if __name__ == '__main__':
+    unittest.main()
