ebd65633b1449c8e427ba9edb1f1d94308e78dd9
Adriane M Hanson <ahansonuga@gmail.com>
2026-01-09 10:29:29 -0500

    Move lower bar explanation to docstring

--- css_archiving_format.py
+++ css_archiving_format.py
@@ -391,7 +391,9 @@ def find_appraisal_rows(df, output_dir):
 
 def find_casework_rows(df):
     """Find metadata rows with topics or text that indicate they are casework and return as a df
-    Once a row matches one pattern, it is not considered for other patterns."""
+    Once a row matches one pattern, it is not considered for other patterns.
+    We will delete even if the phrase indicates it is not a case or casework
+    because the fact they considered it might be a case suggests it includes sensitive personal information."""
 
     # Column in_topic includes one or more of the topics that indicate casework.
     topics_list = ['case work', 'casework', 'prison case']
@@ -411,8 +413,6 @@ def find_casework_rows(df):
     df = df[~out_text]
 
     # Any column includes a phrase that indicates casework.
-    # We will delete even if the phrase indicates it is not a case or casework
-    # because the fact they considered it might be a case suggests it includes sensitive personal information.
     case_list = ['added to case', 'already open', 'case closed', 'case for', 'case has been opened', 'case issue',
                  'case work', 'casework', 'closed case', 'open case', 'started case']
     case_phrase = np.column_stack([df[col].str.contains('|'.join(case_list), case=False, na=False) for col in df])
