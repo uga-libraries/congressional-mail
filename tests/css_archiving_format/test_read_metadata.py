ae80938248f1c8db9f120abce6df37af28233964
Adriane M Hanson <ahansonuga@gmail.com>
2026-01-09 08:38:31 -0500

    Test for in and out document name

--- tests/css_archiving_format/test_read_metadata.py
+++ tests/css_archiving_format/test_read_metadata.py
@@ -18,7 +18,7 @@ def df_to_list(df):
 class MyTestCase(unittest.TestCase):
 
     def test_correct(self):
-        """Test for when the DAT file can be read in its entirety"""
+        """Test for when the DAT file has no errors, no blank rows, and no delimited doc columns"""
         md_df = read_metadata(os.path.join('test_data', 'read_metadata', 'correct.dat'))
 
         # Tests the values in the returned dataframe are correct.
@@ -38,7 +38,7 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(expected, result, "Problem with test for correct")
 
     def test_correct_blanks(self):
-        """Test for when the DAT file can be read in its entirety but has blank rows to skip"""
+        """Test for when the DAT file has blank rows to skip"""
         md_df = read_metadata(os.path.join('test_data', 'read_metadata', 'correct.dat'))
 
         # Tests the values in the returned dataframe are correct.
@@ -58,7 +58,7 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(expected, result, "Problem with test for correct - blanks")
 
     def test_correct_multiple_in(self):
-        """Test for when the DAT file can be read in its entirety and has in_document_name with multiple files"""
+        """Test for when the DAT file has delimiters within in_document_name"""
         md_df = read_metadata(os.path.join('test_data', 'read_metadata', 'correct_multiple_in.dat'))
 
         # Tests the values in the returned dataframe are correct.
@@ -97,8 +97,64 @@ class MyTestCase(unittest.TestCase):
                      'T3', 'blank', 'formC.doc', 'replyC2']]
         self.assertEqual(expected, result, "Problem with test for correct_multiple_in")
 
+    def test_correct_multiple_in_out(self):
+        """Test for when the DAT file has delimiters within in_document_name and out_document_name"""
+        md_df = read_metadata(os.path.join('test_data', 'read_metadata', 'correct_multiple_in_out.dat'))
+
+        # Tests the values in the returned dataframe are correct.
+        result = df_to_list(md_df)
+        expected = [['prefix', 'first', 'middle', 'last', 'suffix', 'appellation', 'title', 'org', 'addr1', 'addr2',
+                     'addr3', 'addr4', 'city', 'state', 'zip', 'country', 'in_id', 'in_type', 'in_method', 'in_date',
+                     'in_topic', 'in_text', 'in_document_name', 'in_fillin', 'out_id', 'out_type', 'out_method',
+                     'out_date', 'out_topic', 'out_text', 'out_document_name', 'out_fillin'],
+                    ['Ms.', 'Anna', 'A.', 'Anderson', 'blank', 'MD', 'blank', 'blank', '123 A St', 'blank', 'blank',
+                     'blank', 'A city', 'AL', '12345', 'US', 'a100', 'General', 'Email', '20240101', 'A1', 'Note',
+                     r'\\smith-atl\dos\public\objects\A.doc', 'text', 'r100', 'General', 'Email', '20240111',
+                     'T1', 'note', r'\\smith-atl\dos\public\form\A1.txt', 'replyA1'],
+                    ['Mr.', 'Bill', 'B.', 'Blue', 'blank', 'blank', 'blank', 'blank', '456 B St', 'Apt 7', 'blank',
+                     'blank', 'B city', 'WY', '23456', 'US', 'b200', 'Case', 'Email', '20240202', 'B1^B2', 'Note',
+                     r'\\smith-atl\dos\public\objects\B.doc', 'blank', 'r200', 'Case', 'Email', '20240212',
+                     'T2', 'note', r'\\smith-atl\dos\public\form\FileB1.txt', 'replyB200'],
+                    ['Mr.', 'Bill', 'B.', 'Blue', 'blank', 'blank', 'blank', 'blank', '456 B St', 'Apt 7', 'blank',
+                     'blank', 'B city', 'WY', '23456', 'US', 'b200', 'Case', 'Email', '20240202', 'B1^B2', 'Note',
+                     r'\\smith-atl\dos\public\objects\B.doc', 'blank', 'r200', 'Case', 'Email', '20240212',
+                     'T2', 'note', r'\\smith-atl\dos\public\form\FileB2.txt', 'replyB200'],
+                    ['Mr.', 'Bill', 'B.', 'Blue', 'blank', 'blank', 'blank', 'blank', '456 B St', 'Apt 7', 'blank',
+                     'blank', 'B city', 'WY', '23456', 'US', 'b200', 'Case', 'Email', '20240202', 'B1^B2', 'Note',
+                     r'\\smith-atl\dos\public\objects\BB.doc', 'blank', 'r200', 'Case', 'Email', '20240212',
+                     'T2', 'note', r'\\smith-atl\dos\public\form\FileB1.txt', 'replyB200'],
+                    ['Mr.', 'Bill', 'B.', 'Blue', 'blank', 'blank', 'blank', 'blank', '456 B St', 'Apt 7', 'blank',
+                     'blank', 'B city', 'WY', '23456', 'US', 'b200', 'Case', 'Email', '20240202', 'B1^B2', 'Note',
+                     r'\\smith-atl\dos\public\objects\BB.doc', 'blank', 'r200', 'Case', 'Email', '20240212',
+                     'T2', 'note', r'\\smith-atl\dos\public\form\FileB2.txt', 'replyB200'],
+                    ['Ms.', 'CeCe', 'C.', 'Cleese', 'blank', 'blank', 'blank', 'blank', '789 C St', 'Apt C', 'blank',
+                     'blank', 'C city', 'CA', '78911', 'US', 'c300', 'General', 'Email', '20240303', 'C1', 'Note',
+                     r'\\smith-atl\dos\public\objects\C.doc', 'text', 'r300', 'General', 'Email', '20240313',
+                     'T3', 'note', r'\\smith-atl\dos\public\form\C1.txt', 'replyC2'],
+                    ['Ms.', 'CeCe', 'C.', 'Cleese', 'blank', 'blank', 'blank', 'blank', '789 C St', 'Apt C', 'blank',
+                     'blank', 'C city', 'CA', '78911', 'US', 'c300', 'General', 'Email', '20240303', 'C1', 'Note',
+                     r'\\smith-atl\dos\public\objects\C.doc', 'text', 'r300', 'General', 'Email', '20240313',
+                     'T3', 'note', r'\\smith-atl\dos\public\form\C2.txt', 'replyC2'],
+                    ['Ms.', 'CeCe', 'C.', 'Cleese', 'blank', 'blank', 'blank', 'blank', '789 C St', 'Apt C', 'blank',
+                     'blank', 'C city', 'CA', '78911', 'US', 'c300', 'General', 'Email', '20240303', 'C1', 'Note',
+                     r'\\smith-atl\dos\public\objects\CC.doc', 'text', 'r300', 'General', 'Email', '20240313',
+                     'T3', 'note', r'\\smith-atl\dos\public\form\C1.txt', 'replyC2'],
+                    ['Ms.', 'CeCe', 'C.', 'Cleese', 'blank', 'blank', 'blank', 'blank', '789 C St', 'Apt C', 'blank',
+                     'blank', 'C city', 'CA', '78911', 'US', 'c300', 'General', 'Email', '20240303', 'C1', 'Note',
+                     r'\\smith-atl\dos\public\objects\CC.doc', 'text', 'r300', 'General', 'Email', '20240313',
+                     'T3', 'note', r'\\smith-atl\dos\public\form\C2.txt', 'replyC2'],
+                    ['Ms.', 'CeCe', 'C.', 'Cleese', 'blank', 'blank', 'blank', 'blank', '789 C St', 'Apt C', 'blank',
+                     'blank', 'C city', 'CA', '78911', 'US', 'c300', 'General', 'Email', '20240303', 'C1', 'Note',
+                     r'\\smith-atl\dos\public\objects\CCC.doc', 'text', 'r300', 'General', 'Email', '20240313',
+                     'T3', 'note', r'\\smith-atl\dos\public\form\C1.txt', 'replyC2'],
+                    ['Ms.', 'CeCe', 'C.', 'Cleese', 'blank', 'blank', 'blank', 'blank', '789 C St', 'Apt C', 'blank',
+                     'blank', 'C city', 'CA', '78911', 'US', 'c300', 'General', 'Email', '20240303', 'C1', 'Note',
+                     r'\\smith-atl\dos\public\objects\CCC.doc', 'text', 'r300', 'General', 'Email', '20240313',
+                     'T3', 'note', r'\\smith-atl\dos\public\form\C2.txt', 'replyC2']]
+        self.assertEqual(expected, result, "Problem with test for correct_multiple_in_out")
+
     def test_correct_multiple_out(self):
-        """Test for when the DAT file can be read in its entirety and has out_document_name with multiple files"""
+        """Test for when the DAT file has delimiters within out_document_name"""
         md_df = read_metadata(os.path.join('test_data', 'read_metadata', 'correct_multiple_out.dat'))
 
         # Tests the values in the returned dataframe are correct.
