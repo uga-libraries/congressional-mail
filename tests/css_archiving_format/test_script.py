21de44ebcf365d6645a1454b2e31e848f7283750
Adriane M Hanson <ahansonuga@gmail.com>
2026-01-21 13:38:06 -0500

    New appraisal test if no csv

--- tests/css_archiving_format/test_script.py
+++ tests/css_archiving_format/test_script.py
@@ -538,6 +538,25 @@ class MyTestCase(unittest.TestCase):
         expected = "Missing required arguments, input_directory and script_mode\r\n"
         self.assertEqual(expected, result, "Problem with test for error argument, printed error")
 
+    def test_error_appraisal_no_delete(self):
+        """Test for when the script exits due to a missing appraisal_delete_log."""
+        script_path = os.path.join(os.getcwd(), '..', '..', 'css_archiving_format.py')
+        input_directory = os.path.join('test_data', 'script', 'Incomplete_Export')
+
+        # Runs the script and tests that it exits.
+        with self.assertRaises(subprocess.CalledProcessError):
+            subprocess.run(f"python {script_path} {input_directory} appraisal",
+                           shell=True, check=True, stdout=subprocess.PIPE)
+
+        # Runs the script and tests that it prints the correct error.
+        output = subprocess.run(f"python {script_path} {input_directory} appraisal", shell=True, stdout=subprocess.PIPE)
+        result = output.stdout.decode('utf-8')
+        expected = ("\r\nThe script is running in appraisal mode.\r\n"
+                    "It will delete letters due to appraisal and make a report of metadata to review for restrictions,"
+                    "but not change the metadata file.\r\n"
+                    "No appraisal_delete_log.csv in the output directory. Cannot do appraisal without it.\r\n")
+        self.assertEqual(expected, result, "Problem with test for error argument, printed error")
+
 
 if __name__ == '__main__':
     unittest.main()
