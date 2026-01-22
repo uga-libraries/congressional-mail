f6c9fafb970a75994b3f04a4cd239be1b15090ae
Adriane M Hanson <ahansonuga@gmail.com>
2026-01-22 10:02:33 -0500

    Update tests to match new parameter

--- tests/css_archiving_format/test_remove_restricted_rows.py
+++ tests/css_archiving_format/test_remove_restricted_rows.py
@@ -1,4 +1,3 @@
-import os
 import pandas as pd
 import unittest
 from css_archiving_format import remove_restricted_rows
@@ -9,59 +8,71 @@ class MyTestCase(unittest.TestCase):
 
     def test_delimiter(self):
         """Test for when columns in md_df have delimiters in the topic columns"""
-        # Makes dataframe to use as test input and runs the function.
+        # Makes dataframes to use as test input and runs the function.
         md_df = pd.DataFrame([['Anders', '12345', 'citizen^open', r'..\objects\1.txt', 'open'],
                               ['Blooms', '23456', 'citizen^refugee', r'..\objects\2.txt', 'open'],
                               ['Cliver', '34567', 'court', r'..\objects\3.txt', 'immigrant^immigration'],
                               ['Dudley', '45678', 'citizen^migrant', r'..\objects\4.txt', 'court^citizen'],
                               ['Everly', '56789', 'open_ok', r'..\objects\5.txt', 'response']],
                              columns=['last', 'zip', 'in_topic', 'in_document_name', 'out_topic'])
-        output_directory = os.path.join('test_data', 'remove_restricted_rows', 'delimiter')
-        md_df = remove_restricted_rows(md_df, output_directory)
+        restrict_df = pd.DataFrame([['Anders', '12345', 'citizen^open', r'..\objects\1.txt', 'open', 'citizen', 'open'],
+                                    ['Blooms', '23456', 'citizen^refugee', r'..\objects\2.txt', 'open', 'citizen', 'open'],
+                                    ['Blooms', '23456', 'citizen^refugee', r'..\objects\2.txt', 'open', 'refugee', 'open'],
+                                    ['Cliver', '34567', 'court', r'..\objects\3.txt', 'immigrant^immigration', 'court', 'immigrant'],
+                                    ['Cliver', '34567', 'court', r'..\objects\3.txt', 'immigrant^immigration', 'court', 'migration'],
+                                    ['Dudley', '45678', 'citizen^migrant', r'..\objects\4.txt', 'court^citizen', 'citizen', 'court'],
+                                    ['Dudley', '45678', 'citizen^migrant', r'..\objects\4.txt', 'court^citizen', 'citizen', 'citizen'],
+                                    ['Dudley', '45678', 'citizen^migrant', r'..\objects\4.txt', 'court^citizen', 'migrant', 'court'],
+                                    ['Dudley', '45678', 'citizen^migrant', r'..\objects\4.txt', 'court^citizen', 'migrant', 'citizen']],
+                                   columns=['last', 'zip', 'in_topic', 'in_document_name', 'out_topic', 'in_topic_split', 'out_topic_split'])
+        md_df = remove_restricted_rows(md_df, restrict_df)
 
         # Tests the values in the returned dataframe are correct.
         result = df_to_list(md_df)
         expected = [['last', 'zip', 'in_topic', 'in_document_name', 'out_topic'],
                     ['Everly', '56789', 'open_ok', r'..\objects\5.txt', 'response']]
-        self.assertEqual(expected, result, "Problem with test for type_same")
+        self.assertEqual(expected, result, "Problem with test for delimiter")
 
-    def test_type_different(self):
-        """Test for when columns in md_df have a different datatype as restriction_review.csv"""
-        # Makes dataframe to use as test input and runs the function.
-        md_df = pd.DataFrame([['Anders', 12345, 'citizen', r'..\objects\1.txt', 'response'],
-                              ['Blooms', 23456, 'citizen', r'..\objects\2.txt', 'response'],
-                              ['Cliver', 34567, 'open_ok', r'..\objects\3.txt', 'response'],
-                              ['Dudley', 45678, 'migrant', r'..\objects\4.txt', 'response'],
-                              ['Everly', 56789, 'open_ok', r'..\objects\5.txt', 'response']],
-                             columns=['last', 'zip', 'in_topic', 'in_document_name', 'out_topic'])
-        output_directory = os.path.join('test_data', 'remove_restricted_rows', 'no_delimiter')
-        md_df = remove_restricted_rows(md_df, output_directory)
+    # def test_type_different(self):
+    #     """Test for when columns in md_df have a different datatype as restriction_review.csv"""
+    #     # Makes dataframe to use as test input and runs the function.
+    #     md_df = pd.DataFrame([['Anders', 12345, 'citizen', r'..\objects\1.txt', 'response'],
+    #                           ['Blooms', 23456, 'citizen', r'..\objects\2.txt', 'response'],
+    #                           ['Cliver', 34567, 'open_ok', r'..\objects\3.txt', 'response'],
+    #                           ['Dudley', 45678, 'migrant', r'..\objects\4.txt', 'response'],
+    #                           ['Everly', 56789, 'open_ok', r'..\objects\5.txt', 'response']],
+    #                          columns=['last', 'zip', 'in_topic', 'in_document_name', 'out_topic'])
+    #     output_directory = os.path.join('test_data', 'remove_restricted_rows', 'no_delimiter')
+    #     md_df = remove_restricted_rows(md_df, output_directory)
+    #
+    #     # Tests the values in the returned dataframe are correct.
+    #     result = df_to_list(md_df)
+    #     expected = [['last', 'zip', 'in_topic', 'in_document_name', 'out_topic'],
+    #                 ['Cliver', '34567', 'open_ok', r'..\objects\3.txt', 'response'],
+    #                 ['Everly', '56789', 'open_ok', r'..\objects\5.txt', 'response']]
+    #     self.assertEqual(expected, result, "Problem with test for type_different")
 
-        # Tests the values in the returned dataframe are correct.
-        result = df_to_list(md_df)
-        expected = [['last', 'zip', 'in_topic', 'in_document_name', 'out_topic'],
-                    ['Cliver', '34567', 'open_ok', r'..\objects\3.txt', 'response'],
-                    ['Everly', '56789', 'open_ok', r'..\objects\5.txt', 'response']]
-        self.assertEqual(expected, result, "Problem with test for type_different")
-
-    def test_type_same(self):
+    def test_no_delimiter(self):
         """Test for when columns in md_df have the same datatype as restriction_review.csv"""
-        # Makes dataframe to use as test input and runs the function.
+        # Makes dataframes to use as test input and runs the function.
         md_df = pd.DataFrame([['Anders', '12345', 'citizen', r'..\objects\1.txt', 'response'],
                               ['Blooms', '23456', 'citizen', r'..\objects\2.txt', 'response'],
                               ['Cliver', '34567', 'open_ok', r'..\objects\3.txt', 'response'],
                               ['Dudley', '45678', 'migrant', r'..\objects\4.txt', 'response'],
                               ['Everly', '56789', 'open_ok', r'..\objects\5.txt', 'response']],
                              columns=['last', 'zip', 'in_topic', 'in_document_name', 'out_topic'])
-        output_directory = os.path.join('test_data', 'remove_restricted_rows', 'no_delimiter')
-        md_df = remove_restricted_rows(md_df, output_directory)
+        restrict_df = pd.DataFrame([['Anders', '12345', 'citizen', r'..\objects\1.txt', 'response', 'citizen', 'response'],
+                                    ['Blooms', '23456', 'citizen', r'..\objects\2.txt', 'response', 'citizen', 'response'],
+                                    ['Dudley', '45678', 'migrant', r'..\objects\4.txt', 'response', 'migrant', 'response']],
+                                   columns=['last', 'zip', 'in_topic', 'in_document_name', 'out_topic', 'in_topic_split', 'out_topic_split'])
+        md_df = remove_restricted_rows(md_df, restrict_df)
 
         # Tests the values in the returned dataframe are correct.
         result = df_to_list(md_df)
         expected = [['last', 'zip', 'in_topic', 'in_document_name', 'out_topic'],
                     ['Cliver', '34567', 'open_ok', r'..\objects\3.txt', 'response'],
                     ['Everly', '56789', 'open_ok', r'..\objects\5.txt', 'response']]
-        self.assertEqual(expected, result, "Problem with test for type_same")
+        self.assertEqual(expected, result, "Problem with test for no_delimiter")
 
 
 if __name__ == '__main__':
