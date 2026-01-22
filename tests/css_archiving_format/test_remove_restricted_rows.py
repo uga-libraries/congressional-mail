3a1c35e0f0f597d50f5a234fa046d033b2b84d67
Adriane M Hanson <ahansonuga@gmail.com>
2026-01-22 10:02:44 -0500

    Delete unnecessary diff type test

--- tests/css_archiving_format/test_remove_restricted_rows.py
+++ tests/css_archiving_format/test_remove_restricted_rows.py
@@ -33,25 +33,6 @@ class MyTestCase(unittest.TestCase):
                     ['Everly', '56789', 'open_ok', r'..\objects\5.txt', 'response']]
         self.assertEqual(expected, result, "Problem with test for delimiter")
 
-    # def test_type_different(self):
-    #     """Test for when columns in md_df have a different datatype as restriction_review.csv"""
-    #     # Makes dataframe to use as test input and runs the function.
-    #     md_df = pd.DataFrame([['Anders', 12345, 'citizen', r'..\objects\1.txt', 'response'],
-    #                           ['Blooms', 23456, 'citizen', r'..\objects\2.txt', 'response'],
-    #                           ['Cliver', 34567, 'open_ok', r'..\objects\3.txt', 'response'],
-    #                           ['Dudley', 45678, 'migrant', r'..\objects\4.txt', 'response'],
-    #                           ['Everly', 56789, 'open_ok', r'..\objects\5.txt', 'response']],
-    #                          columns=['last', 'zip', 'in_topic', 'in_document_name', 'out_topic'])
-    #     output_directory = os.path.join('test_data', 'remove_restricted_rows', 'no_delimiter')
-    #     md_df = remove_restricted_rows(md_df, output_directory)
-    #
-    #     # Tests the values in the returned dataframe are correct.
-    #     result = df_to_list(md_df)
-    #     expected = [['last', 'zip', 'in_topic', 'in_document_name', 'out_topic'],
-    #                 ['Cliver', '34567', 'open_ok', r'..\objects\3.txt', 'response'],
-    #                 ['Everly', '56789', 'open_ok', r'..\objects\5.txt', 'response']]
-    #     self.assertEqual(expected, result, "Problem with test for type_different")
-
     def test_no_delimiter(self):
         """Test for when columns in md_df have the same datatype as restriction_review.csv"""
         # Makes dataframes to use as test input and runs the function.
