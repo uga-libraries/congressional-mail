378b367c605705038ae2b5037a5d98250037a419
Adriane M Hanson <ahansonuga@gmail.com>
2026-01-30 13:27:41 -0500

    Use df_search in find and simpler search term

--- css_data_interchange_format.py
+++ css_data_interchange_format.py
@@ -266,32 +266,15 @@ def df_search(df, keywords, category):
 
 
 def find_academy_rows(df):
-    """Find metadata rows with topics or text that indicate they are academy applications and return as a df
-    Once a row matches one pattern, it is not considered for other patterns."""
+    """Find metadata rows with keywords that indicate they might be academy applications
+    and return as two dfs, one with more certainty (df_academy) and one with less (df_academy_check)"""
 
-    # Column group_name includes "academy".
-    group = df['group_name'].str.contains('academy', case=False, na=False)
-    df_group = df[group]
-    df = df[~group]
+    # Makes df with more certainty.
+    df_academy, df_unmatched = df_search(df, 'academy', 'Academy_Application')
 
-    # Column communication_document_name includes "academy".
-    doc_name = df['communication_document_name'].str.contains('academy', case=False, na=False)
-    df_doc_name = df[doc_name]
-    df = df[~doc_name]
-
-    # Column text includes "academy".
-    text = df['text'].str.contains('academy', case=False, na=False)
-    df_text = df[text]
-    df = df[~text]
-
-    # Makes a single dataframe with all rows that indicate academy applications
-    # and adds a column for the appraisal category (needed for the file deletion log).
-    df_academy = pd.concat([df_group, df_doc_name, df_text], axis=0, ignore_index=True)
-    df_academy['Appraisal_Category'] = 'Academy_Application'
-
-    # Makes another dataframe with rows containing "academy" to check for new patterns that could
-    # indicate academy applications.
-    df_academy_check = appraisal_check_df(df, 'academy', 'Academy_Application')
+    # Makes df with less certainty, only searching rows that are not in df_academy, to find for new patterns.
+    # TODO update term now that df_academy is simplified to searching for just academy.
+    df_academy_check, df_unmatched = df_search(df_unmatched, 'academy', 'Academy_Application')
 
     return df_academy, df_academy_check
 
