2a4ce55579cb3517c88708c3824ca55c2f320f6f
Adriane M Hanson <ahansonuga@gmail.com>
2026-02-02 10:20:07 -0500

    Start css_arch - update docstring

--- css_archiving_format.py
+++ css_archiving_format.py
@@ -466,8 +466,8 @@ def find_casework_rows(df):
 
 
 def find_job_rows(df):
-    """Find metadata rows with topics or text that indicate they are job applications and return as a df
-    Once a row matches one pattern, it is not considered for other patterns."""
+    """Find metadata rows with keywords that indicate they might be job applications
+    and return as a two dfs, one with more certain (df_job) and one with less (df_job_check)"""
 
     # Column in_topic includes one or more of the topics that indicate job applications.
     topics_list = ['intern', 'resume']
