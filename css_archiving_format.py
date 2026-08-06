7ac9f5fd4144e1b8b33b2ac0d341d0a473d93ad5
Adriane M Hanson <ahansonuga@gmail.com>
2026-08-06 09:52:39 -0400

    Print statement for acces_restart

--- css_archiving_format.py
+++ css_archiving_format.py
@@ -916,6 +916,9 @@ if __name__ == '__main__':
     # For access_restarts, finishes the topics sort. The rest of access will already be done.
     # Uses topics_sort_metadata.csv and topics_sort_complete.txt (in output_directory from access mode) to restart.
     elif script_mode == 'access_restart':
+        print("\nThe script is running in access_restart mode.")
+        print("It will continue copying the letters to and from constituents organized by topic, "
+              "skipping topics already done prior to the script being stopped.")
         md_df = pd.read_csv(os.path.join(output_directory, 'topics_sort_metadata.csv'), dtype=str)
         topics_sort(md_df, input_directory, output_directory, restart=True)
 
