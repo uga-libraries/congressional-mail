2024a79460cdb200357bf628745152febefe76d6
Adriane M Hanson <ahansonuga@gmail.com>
2026-08-10 09:39:04 -0400

    Update test_topics_sort_delete_empty.py

--- tests/css_archiving_format/test_topics_sort_delete_empty.py
+++ tests/css_archiving_format/test_topics_sort_delete_empty.py
@@ -2,72 +2,75 @@ import os
 import shutil
 import unittest
 from css_archiving_format import topics_sort_delete_empty
-
-
-def check_paths():
-    """Checks if the three paths exist and returns as a list of Booleans"""
-    topic_path = os.path.join('test_data', 'topics_sort_delete_empty', 'topic')
-    check = [os.path.exists(topic_path),
-             os.path.exists(os.path.join(topic_path, 'from_constituents')),
-             os.path.exists(os.path.join(topic_path, 'to_constituents'))]
-    return check
+from test_script import make_dir_list
 
 
 class MyTestCase(unittest.TestCase):
 
     def tearDown(self):
         """Delete the copy of the test data, if made and not completely deleted by the test"""
-        test_data = os.path.join('test_data', 'topics_sort_delete_empty', 'topic')
+        test_data = os.path.join('test_data', 'topics_sort_delete_empty', 'by_topic')
         if os.path.exists(test_data):
             shutil.rmtree(test_data)
 
-    def test_both_empty(self):
-        """Test for when both to and from folders are empty, so the topic is also empty"""
+    def test_empty_topic(self):
+        """Test for when the topic folder is empty and is deleted"""
         # Copies the test data, because the function alters it, and runs the function.
-        shutil.copytree(os.path.join('test_data', 'topics_sort_delete_empty', 'both_empty'),
-                        os.path.join('test_data', 'topics_sort_delete_empty', 'topic'))
-        topics_sort_delete_empty(os.path.join(os.getcwd(), 'test_data', 'topics_sort_delete_empty', 'topic'))
+        by_topic_path = os.path.join('test_data', 'topics_sort_delete_empty', 'by_topic')
+        shutil.copytree(os.path.join('test_data', 'topics_sort_delete_empty', 'empty_topic'), by_topic_path)
+        topics_sort_delete_empty(by_topic_path)
 
-        # Verifies all folders were deleted.
-        result = check_paths()
-        expected = [False, False, False]
-        self.assertEqual(expected, result, "Problem with test for both_empty")
+        # Checks the contents of by_topic to verify the correct folders were deleted.
+        result = make_dir_list(by_topic_path)
+        expected = []
+        self.assertEqual(expected, result, "Problem with test for empty_topic")
 
-    def test_from_empty(self):
-        """Test for when the from_constituents folder is empty"""
+    def test_empty_type(self):
+        """Test for when the type folder within a topic folder is empty and is deleted"""
         # Copies the test data, because the function alters it, and runs the function.
-        shutil.copytree(os.path.join('test_data', 'topics_sort_delete_empty', 'from_empty'),
-                        os.path.join('test_data', 'topics_sort_delete_empty', 'topic'))
-        topics_sort_delete_empty(os.path.join('test_data', 'topics_sort_delete_empty', 'topic'))
+        by_topic_path = os.path.join('test_data', 'topics_sort_delete_empty', 'by_topic')
+        shutil.copytree(os.path.join('test_data', 'topics_sort_delete_empty', 'empty_type'), by_topic_path)
+        topics_sort_delete_empty(by_topic_path)
 
-        # Verifies all folders were deleted.
-        result = check_paths()
-        expected = [True, False, True]
-        self.assertEqual(expected, result, "Problem with test for from_empty")
+        # Checks the contents of by_topic to verify the correct folders were deleted.
+        result = make_dir_list(by_topic_path)
+        expected = [os.path.join(by_topic_path, 'banana'),
+                    os.path.join(by_topic_path, 'banana', 'objects'),
+                    os.path.join(by_topic_path, 'banana', 'objects', 'letter.txt')]
+        self.assertEqual(expected, result, "Problem with test for empty_type")
 
-    def test_neither_empty(self):
-        """Test for when to and from folders are not empty, so nothing is deleted"""
+    def test_empty_type_subfolder(self):
+        """Test for when the subfolder within a type folder is empty and is deleted"""
         # Copies the test data, because the function alters it, and runs the function.
-        shutil.copytree(os.path.join('test_data', 'topics_sort_delete_empty', 'neither_empty'),
-                        os.path.join('test_data', 'topics_sort_delete_empty', 'topic'))
-        topics_sort_delete_empty(os.path.join('test_data', 'topics_sort_delete_empty', 'topic'))
+        by_topic_path = os.path.join('test_data', 'topics_sort_delete_empty', 'by_topic')
+        shutil.copytree(os.path.join('test_data', 'topics_sort_delete_empty', 'empty_type_subfolder'), by_topic_path)
+        topics_sort_delete_empty(by_topic_path)
 
-        # Verifies all folders were deleted.
-        result = check_paths()
-        expected = [True, True, True]
-        self.assertEqual(expected, result, "Problem with test for neither_empty")
+        # Checks the contents of by_topic to verify the correct folders were deleted.
+        result = make_dir_list(by_topic_path)
+        expected = [os.path.join(by_topic_path, 'banana'),
+                    os.path.join(by_topic_path, 'banana', 'form'),
+                    os.path.join(by_topic_path, 'banana', 'form', 'dessert'),
+                    os.path.join(by_topic_path, 'banana', 'form', 'dessert', 'letter.txt')]
+        self.assertEqual(expected, result, "Problem with test for empty_type_subfolder")
 
-    def test_to_empty(self):
-        """Test for when the to_constituents folder is empty"""
+    def test_no_empty(self):
+        """Test for when no folders are empty, so nothing is deleted"""
         # Copies the test data, because the function alters it, and runs the function.
-        shutil.copytree(os.path.join('test_data', 'topics_sort_delete_empty', 'to_empty'),
-                        os.path.join('test_data', 'topics_sort_delete_empty', 'topic'))
-        topics_sort_delete_empty(os.path.join('test_data', 'topics_sort_delete_empty', 'topic'))
+        by_topic_path = os.path.join('test_data', 'topics_sort_delete_empty', 'by_topic')
+        shutil.copytree(os.path.join('test_data', 'topics_sort_delete_empty', 'no_empty'), by_topic_path)
+        topics_sort_delete_empty(by_topic_path)
 
-        # Verifies all folders were deleted.
-        result = check_paths()
-        expected = [True, True, False]
-        self.assertEqual(expected, result, "Problem with test for to_empty")
+        # Checks the contents of by_topic to verify no folders were deleted.
+        result = make_dir_list(by_topic_path)
+        expected = [os.path.join(by_topic_path, 'apple'),
+                    os.path.join(by_topic_path, 'banana'),
+                    os.path.join(by_topic_path, 'apple', 'form'),
+                    os.path.join(by_topic_path, 'apple', 'form', 'letter.txt'),
+                    os.path.join(by_topic_path, 'banana', 'form'),
+                    os.path.join(by_topic_path, 'banana', 'form', 'pro'),
+                    os.path.join(by_topic_path, 'banana', 'form', 'pro', 'letter.txt')]
+        self.assertEqual(expected, result, "Problem with test for no_empty")
 
 
 if __name__ == '__main__':
